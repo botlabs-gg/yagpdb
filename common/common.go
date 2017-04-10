5c2188570eeab734cf520a691ea256bf36e3d4b7
jonas747 <jonasr747@gmail.com>
2017-04-11 00:15:50 +0200

    Update version

--- common/common.go
+++ common/common.go
@@ -12,7 +12,7 @@ import (
 const (
 	VERSIONMAJOR = 0
 	VERSIONMINOR = 19
-	VERSIONPATCH = 5
+	VERSIONPATCH = 6
 
 	Testing = false // Disables stuff like command cooldowns
 	// Testing = true // Disables stuff like command cooldowns
