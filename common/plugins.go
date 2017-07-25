f6953118a98116c29cdb7c1a58e8b5b82228909b
jonas747 <jonasr747@gmail.com>
2017-07-25 04:34:47 +0200

    Try out new plugin logger system

--- common/plugins.go
+++ common/plugins.go
@@ -1,5 +1,9 @@
 package common
 
+import (
+	"github.com/Sirupsen/logrus"
+)
+
 var (
 	Plugins []Plugin
 )
@@ -9,7 +13,40 @@ type Plugin interface {
 	Name() string
 }
 
+type PluginWithLogging interface {
+	Logger() *logrus.Entry
+	SetLogger(entry *logrus.Entry)
+}
+
 // RegisterPlugin registers a plugin, should be called when the bot is starting up
 func RegisterPlugin(plugin Plugin) {
 	Plugins = append(Plugins, plugin)
+	if cast, ok := plugin.(PluginWithLogging); ok {
+		cast.SetLogger(logrus.WithField("P", plugin.Name()))
+	} else {
+		logrus.Warn(plugin.Name(), " is not a PluginWithLogging")
+	}
+}
+
+// RegisterPluginL registers a plugin, should be called when the bot is starting up
+func RegisterPluginL(pl Plugin) {
+	if _, ok := pl.(PluginWithLogging); !ok {
+		logrus.Fatal("Not a PluginWithLogging: ", pl.Name())
+	}
+
+	RegisterPlugin(pl)
+}
+
+type BasePlugin struct {
+	Entry *logrus.Entry
+}
+
+var _ PluginWithLogging = (*BasePlugin)(nil)
+
+func (p *BasePlugin) Logger() *logrus.Entry {
+	return p.Entry
+}
+
+func (p *BasePlugin) SetLogger(entry *logrus.Entry) {
+	p.Entry = entry
 }
