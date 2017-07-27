f3274613ad85c8e92b8e4ec5a192dfe115e7e1ed
jonas747 <jonasr747@gmail.com>
2017-07-27 08:22:42 +0200

    Add the ability to ban servers

--- bot/discordevents.go
+++ bot/discordevents.go
@@ -44,6 +44,13 @@ func HandleGuildCreate(evt *eventsystem.EventData) {
 			Type: eventsystem.EventNewGuild,
 		}, eventsystem.EventNewGuild)
 	}
+
+	banned, _ := ContextRedis(evt.Context()).Cmd("SISMEMBER", "banned_servers", g.ID).Bool()
+	if banned {
+		log.WithField("guild", g.ID).Info("Banned server tried to add bot back")
+		common.BotSession.ChannelMessageSend(g.ID, "This server is banned from using this bot. Join the support server for more info.")
+		common.BotSession.GuildLeave(g.ID)
+	}
 }
 
 func HandleGuildDelete(evt *eventsystem.EventData) {
