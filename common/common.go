75fc6d0389f23cb8d6bb08da8700fdb590b092b9
jonas747 <jonasr747@gmail.com>
2017-08-08 04:59:34 +0200

    Update version and start work towards 0.24

--- common/common.go
+++ common/common.go
@@ -15,15 +15,15 @@ import (
 
 const (
 	VERSIONMAJOR = 0
-	VERSIONMINOR = 23
+	VERSIONMINOR = 24
 	VERSIONPATCH = 0
 
-	Testing = false // Disables stuff like command cooldowns
+	Testing = true // Disables stuff like command cooldowns
 )
 
 var (
 	VERSIONNUMBER = fmt.Sprintf("%d.%d.%d", VERSIONMAJOR, VERSIONMINOR, VERSIONPATCH)
-	VERSION       = VERSIONNUMBER + " Lawless"
+	VERSION       = VERSIONNUMBER + " Dev"
 
 	GORM        *gorm.DB
 	PQ          *sql.DB
