29fff1da74bc5261bd539dd130a030aa3d0ed751
jonas747 <jonasr747@gmail.com>
2018-02-13 19:32:20 +0100

    Add Urban dictionary command

--- stdcommands/general_commands.go
+++ stdcommands/general_commands.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/alfredxing/calc/compute"
+	"github.com/dpatrie/urbandictionary"
 	"github.com/jonas747/dcmd"
 	"github.com/jonas747/dice"
 	"github.com/jonas747/discordgo"
@@ -38,6 +39,7 @@ type PluginStatus interface {
 }
 
 var generalCommands = []*commands.YAGCommand{
+	cmdDefine,
 	cmdReverse,
 	cmdWeather,
 	cmdCalc,
@@ -457,6 +459,37 @@ var cmdWouldYouRather = &commands.YAGCommand{
 		}
 
 		return "", nil
+	},
+}
+
+var cmdDefine = &commands.YAGCommand{
+	CmdCategory:  commands.CategoryFun,
+	Name:         "Define",
+	Aliases:      []string{"df"},
+	Description:  "Look up an urban dictionary definition",
+	RequiredArgs: 1,
+	Arguments: []*dcmd.ArgDef{
+		{Name: "Topic", Type: dcmd.String},
+	},
+	RunFunc: func(data *dcmd.Data) (interface{}, error) {
+
+		qResp, err := urbandictionary.Query(data.Args[0].Str())
+		if err != nil {
+			return "Failed querying :(", err
+		}
+
+		if len(qResp.Results) < 1 {
+			return "No result :(", nil
+		}
+
+		result := qResp.Results[0]
+
+		cmdResp := fmt.Sprintf("**%s**: %s\n*%s*\n*(<%s>)*", result.Word, result.Definition, result.Example, result.Permalink)
+		if len(qResp.Results) > 1 {
+			cmdResp += fmt.Sprintf(" *%d more results*", len(qResp.Results)-1)
+		}
+
+		return cmdResp, nil
 	},
 }
 
