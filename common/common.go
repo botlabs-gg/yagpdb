4d78eb5a6f0271ef0bb0e2e1cfb7a7943f2f7f60
jonas747 <jonasr747@gmail.com>
2018-03-03 13:39:01 +0100

    Disable the fixed pool

--- common/common.go
+++ common/common.go
@@ -7,7 +7,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/jonas747/discordgo"
-	"github.com/jonas747/yagpdb/common/fixedpool"
+	"github.com/mediocregopher/radix.v2/pool"
 	boilOld "github.com/vattle/sqlboiler/boil"
 	"github.com/volatiletech/sqlboiler/boil"
 	stdlog "log"
