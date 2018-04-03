73ae1b7211594ea697dfe7d98f6f1e91c01297ea
jonas747 <jonasr747@gmail.com>
2018-04-03 21:08:23 +0200

    Fix selected channel in multi channel dropdowns sometimes not showing

--- autorole/autorole.go
+++ autorole/autorole.go
@@ -3,9 +3,11 @@ package autorole
 //go:generate esc -o assets_gen.go -pkg autorole -ignore ".go" assets/
 
 import (
+	"encoding/json"
 	"github.com/jonas747/discordgo"
 	"github.com/jonas747/yagpdb/common"
 	"github.com/mediocregopher/radix.v2/redis"
+	"strconv"
 )
 
 func KeyGeneral(guildID int64) string { return "autorole:" + discordgo.StrID(guildID) + ":general" }
@@ -31,6 +33,31 @@ type GeneralConfig struct {
 	RequiredRoles []int64 `valid:"role,true"`
 	IgnoreRoles   []int64 `valid:"role,true"`
 	OnlyOnJoin    bool
+}
+
+type LegacyGeneralConfig struct {
+	Role             string `valid:"role,true"`
+	RequiredDuration int
+
+	RequiredRoles []int64 `valid:"role,true"`
+	IgnoreRoles   []int64 `valid:"role,true"`
+	OnlyOnJoin    bool
+}
+
+func (l *GeneralConfig) UnmarshalJSON(b []byte) error {
+	var tmp LegacyGeneralConfig
+	err := json.Unmarshal(b, &tmp)
+	if err != nil {
+		return err
+	}
+
+	l.Role, _ = strconv.ParseInt(tmp.Role, 10, 64)
+	l.RequiredDuration = tmp.RequiredDuration
+	l.RequiredRoles = tmp.RequiredRoles
+	l.IgnoreRoles = tmp.IgnoreRoles
+	l.OnlyOnJoin = tmp.OnlyOnJoin
+
+	return nil
 }
 
 func GetGeneralConfig(client *redis.Client, guildID int64) (*GeneralConfig, error) {
