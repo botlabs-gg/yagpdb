7400b3dfd208a02aff9e7536d885f7d7279f818a
jonas747 <jonasr747@gmail.com>
2018-10-13 06:04:20 +0200

    automodv2: fix spamtrigger counting the whole channel

--- automod/triggers.go
+++ automod/triggers.go
@@ -879,6 +879,10 @@ func (spam *SpamTrigger) CheckMessage(ms *dstate.MemberState, cs *dstate.Channel
 			continue
 		}
 
+		if cMsg.Message.Author.ID != m.Author.ID {
+			continue
+		}
+
 		if len(cMsg.Message.Attachments) > 0 {
 			break // treat any attachment as a different message, in the future i may download them and check hash or something? maybe too much
 		}
