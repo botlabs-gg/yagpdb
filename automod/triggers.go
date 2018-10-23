866089b753d39a2fa406e2f10b1ee351d50e86f0
jonas747 <jonasr747@gmail.com>
2018-10-23 20:49:20 +0200

    automodv2: fix safebrowsing not working on links with only 1 threat

--- automod/triggers.go
+++ automod/triggers.go
@@ -559,7 +559,7 @@ func (g *GoogleSafeBrowsingTrigger) CheckMessage(ms *dstate.MemberState, cs *dst
 	}
 
 	for _, link := range urlThreats {
-		if len(link) > 1 {
+		if len(link) > 0 {
 			return true, nil
 		}
 	}
