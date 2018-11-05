0b204cdd9ce0b2530a3257b9fdb8ad4896131d91
jonas747 <jonasr747@gmail.com>
2018-11-05 12:46:17 +0100

    templates: set max write limit to 25kb

--- common/templates/context.go
+++ common/templates/context.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jonas747/dstate"
 	"github.com/jonas747/yagpdb/common"
 	"github.com/pkg/errors"
+	"io"
 	"strings"
 	"text/template"
 )
@@ -161,10 +162,16 @@ func (c *Context) Execute(source string) (string, error) {
 	}
 
 	var buf bytes.Buffer
-	err = parsed.Execute(&buf, c.Data)
+	w := LimitWriter(&buf, 25000)
+
+	err = parsed.Execute(w, c.Data)
 
 	result := common.EscapeSpecialMentionsConditional(buf.String(), c.MentionEveryone, c.MentionHere, c.MentionRoles)
 	if err != nil {
+		if err == io.ErrShortWrite {
+			err = errors.New("response grew too big (>25k)")
+		}
+
 		return result, errors.WithMessage(err, "Failed executing template")
 	}
 
@@ -213,4 +220,32 @@ func baseContextFuncs(c *Context) {
 	c.ContextFuncs["currentUserCreated"] = c.tmplCurrentUserCreated
 	c.ContextFuncs["currentUserAgeHuman"] = c.tmplCurrentUserAgeHuman
 	c.ContextFuncs["currentUserAgeMinutes"] = c.tmplCurrentUserAgeMinutes
+}
+
+type limitedWriter struct {
+	W io.Writer
+	N int64
+}
+
+func (l *limitedWriter) Write(p []byte) (n int, err error) {
+	if l.N <= 0 {
+		return 0, io.ErrShortWrite
+	}
+	if int64(len(p)) > l.N {
+		p = p[0:l.N]
+		err = io.ErrShortWrite
+	}
+	n, er := l.W.Write(p)
+	if er != nil {
+		err = er
+	}
+	l.N -= int64(n)
+	return n, err
+}
+
+// LimitWriter works like io.LimitReader. It writes at most n bytes
+// to the underlying Writer. It returns io.ErrShortWrite if more than n
+// bytes are attempted to be written.
+func LimitWriter(w io.Writer, n int64) io.Writer {
+	return &limitedWriter{W: w, N: n}
 }
