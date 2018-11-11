53056c31becc32896701ccc78b4f1437164aec8d
CHamburr <42373024+CHamburr@users.noreply.github.com>
2018-11-11 16:33:55 +0800

    Invite link blacklist (discordy.com)

--- common/util.go
+++ common/util.go
@@ -23,7 +23,7 @@ func KeyGuild(guildID int64) string         { return "guild:" + discordgo.StrID(
 func KeyGuildChannels(guildID int64) string { return "channels:" + discordgo.StrID(guildID) }
 
 var DiscordInviteRegex = regexp.MustCompile(`(discord\.gg|discordapp\.com\/invite)(?:\/#)?\/([a-zA-Z0-9-]+)`)
-var ThirdPartyDiscordInviteRegex = regexp.MustCompile(`(discord\.me|invite\.gg|discord\.io|disco\.gg|disboard\.org\/server)(?:\/#)?\/([a-zA-Z0-9-]+)`)
+var ThirdPartyDiscordInviteRegex = regexp.MustCompile(`(discord\.me|invite\.gg|discord\.io|disco\.gg|disboard\.org\/server|discordy\.com)(?:\/#)?\/([a-zA-Z0-9-]+)`)
 
 type WrappedGuild struct {
 	*discordgo.UserGuild
