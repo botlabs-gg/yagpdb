03659cbafad15be376287d1d0f767b21d8e1d628
jonas747 <jonasr747@gmail.com>
2018-12-03 21:33:31 +0100

    topservers: switch to use postgres table for top servers

--- stdcommands/topservers/topservers.go
+++ stdcommands/topservers/topservers.go
@@ -3,10 +3,9 @@ package topservers
 import (
 	"fmt"
 	"github.com/jonas747/dcmd"
-	"github.com/jonas747/discordgo"
-	"github.com/jonas747/yagpdb/bot"
+	"github.com/jonas747/yagpdb/bot/models"
 	"github.com/jonas747/yagpdb/commands"
-	"sort"
+	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 var Command = &commands.YAGCommand{
@@ -20,55 +19,15 @@ var Command = &commands.YAGCommand{
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
 		skip := data.Args[0].Int()
 
-		state := bot.State
-		state.RLock()
-
-		guilds := make([]*discordgo.Guild, len(state.Guilds))
-		i := 0
-		for _, v := range state.Guilds {
-			state.RUnlock()
-			guilds[i] = v.LightCopy(true)
-			state.RLock()
-			i++
+		results, err := models.JoinedGuilds(qm.OrderBy("member_count desc"), qm.Limit(20), qm.Offset(skip)).AllG(data.Context())
+		if err != nil {
+			return nil, err
 		}
-		state.RUnlock()
 
-		sortable := GuildsSortUsers(guilds)
-		sort.Sort(sortable)
-
-		entriesIncluded := 0
 		out := "```"
-		for k, v := range sortable {
-			if entriesIncluded > 14 {
-				break
-			}
-
-			if k < skip {
-				continue
-			}
-
-			entriesIncluded++
-			out += fmt.Sprintf("\n#%-2d: %-25s (%d members)", k+1, v.Name, v.MemberCount)
+		for k, v := range results {
+			out += fmt.Sprintf("\n#%-2d: %-25s (%d members)", k+skip+1, v.Name, v.MemberCount)
 		}
-		return "Top servers the bot is on (note: only shows servers on this node):\n" + out + "\n```", nil
+		return "Top servers the bot is on:\n" + out + "\n```", nil
 	},
-}
-
-type GuildsSortUsers []*discordgo.Guild
-
-func (g GuildsSortUsers) Len() int {
-	return len(g)
-}
-
-// Less reports whether the element with
-// index i should sort before the element with index j.
-func (g GuildsSortUsers) Less(i, j int) bool {
-	return g[i].MemberCount > g[j].MemberCount
-}
-
-// Swap swaps the elements with indexes i and j.
-func (g GuildsSortUsers) Swap(i, j int) {
-	temp := g[i]
-	g[i] = g[j]
-	g[j] = temp
 }
