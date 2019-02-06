79be8260be59b84ee779e8c3efcfa6de272c4a0f
NDBroughton <ndbroughton6@gmail.com>
2019-02-05 21:54:50 -0500

    Blaclisted » Blacklisted

--- reputation/reputation.go
+++ reputation/reputation.go
@@ -130,7 +130,7 @@ var (
 	ErrMissingRequiredGiveRole    = UserError("You don't have any of the required roles to give points")
 	ErrMissingRequiredReceiveRole = UserError("Target don't have any of the required roles to receive points")
 
-	ErrBlacklistedGive    = UserError("Blaclisted from giving points")
+	ErrBlacklistedGive    = UserError("Blacklisted from giving points")
 	ErrBlacklistedReceive = UserError("Blacklisted from receiving points")
 	ErrCooldown           = UserError("You're still on cooldown")
 )
