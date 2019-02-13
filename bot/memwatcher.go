37cf85dff1c84baa17637db2f71bc3e934d33b97
jonas747 <jonasr747@gmail.com>
2019-02-13 17:44:16 +0100

    memwatcher: actually include the file

---
+++ bot/memwatcher.go
@@ -0,0 +1,52 @@
+package bot
+
+import (
+	"github.com/shirou/gopsutil/mem"
+	"github.com/sirupsen/logrus"
+	"os"
+	"runtime/debug"
+	"time"
+)
+
+const MemFreeThreshold = 90
+
+type MemWatcher struct {
+	lastTimeFreed time.Time
+}
+
+func watchMemusage() {
+	if os.Getenv("YAGPDB_ENABLE_MEM_MONITOR") == "" || os.Getenv("YAGPDB_ENABLE_MEM_MONITOR") == "no" || os.Getenv("YAGPDB_ENABLE_MEM_MONITOR") == "false" {
+		// not enabled
+		return
+	}
+
+	watcher := &MemWatcher{}
+	go watcher.Run()
+}
+
+func (mw *MemWatcher) Run() {
+	logrus.Info("[mem_monitor] launching memory monitor")
+	ticker := time.NewTicker(time.Second * 10)
+	for {
+		<-ticker.C
+		mw.Check()
+	}
+}
+
+func (mw *MemWatcher) Check() {
+	if time.Since(mw.lastTimeFreed) < time.Minute*10 {
+		return
+	}
+
+	sysMem, err := mem.VirtualMemory()
+	if err != nil {
+		logrus.WithError(err).Error("[mem_monitor] failed retrieving os memory stats")
+		return
+	}
+
+	if sysMem.UsedPercent > MemFreeThreshold {
+		logrus.Info("[mem_monitor] LOW SYSTEM MEMORY, ATTEMPTING TO FREE SOME")
+		debug.FreeOSMemory()
+		mw.lastTimeFreed = time.Now()
+	}
+}
