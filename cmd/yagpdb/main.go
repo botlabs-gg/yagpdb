4339f89e0d9ee6e5d5e2f85376776322c09f07bd
jonas747 <jonasr747@gmail.com>
2019-02-18 01:51:58 +0100

    all: lower redis pool size of bot processes to 25 from 100

--- cmd/yagpdb/main.go
+++ cmd/yagpdb/main.go
@@ -112,9 +112,6 @@ func main() {
 	if flagRunWeb || flagRunEverything {
 		common.RedisPoolSize = 25
 	}
-	if flagRunBot || flagRunEverything {
-		common.RedisPoolSize = 100
-	}
 
 	log.Info("YAGPDB is initializing...")
 
