356015e4400795ce2ed4832529b0f1f64a19b790
caubert <37475633+mrbentarikau@users.noreply.github.com>
2019-04-30 22:30:16 +0300

    suggestion for help command (#437)
    * suggestion for help command
    
    this is one way to fix it when people mistype help argument or even wrong argument and will not get 5/6 DMs every time
    
    * removed newlines
    
    empty lines

--- commands/plugin_bot.go
+++ commands/plugin_bot.go
@@ -193,14 +193,16 @@ func cmdFuncHelp(data *dcmd.Data) (interface{}, error) {
 
 	// Send the targetted help in the channel it was requested in
 	resp = dcmd.GenerateTargettedHelp(target, data, data.ContainerChain[0], &dcmd.StdHelpFormatter{})
-	if len(resp) < 1 {
-		return CmdNotFound(target), nil
-	}
 
-	if len(resp) == 1 {
+	if target != "" {
+		if len(resp) != 1 {
+			// Send command not found in same channel
+			return CmdNotFound(target), nil
+		}
+
 		// Send short help in same channel
-		return resp, nil
-	}
+		return resp, nil 
+    	}
 
 	// Send full help in DM
 	channel, err := common.BotSession.UserChannelCreate(data.Msg.Author.ID)
