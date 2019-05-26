4c0bd0588ba055052b3573d4b03978c92714b11c
caubert <37475633+mrbentarikau@users.noreply.github.com>
2019-05-26 23:03:50 +0300

    usability suggestion for control_panel combined overview (#447)
    * usability suggestion for control_panel stats
    
    Rolecommands plugin has two limits, rolecommands and rolegroups count, they both are capped, It would be more informative to present both of these counts. Dunno whether on not to manually write that current max 1000 there or not,
    
    * added err != nil
    
    Forgot that part, sry... I hope return correct, that part puzzled me a bit.
    
    * second update
    
    that "if err" did not get delivered via pull request update... clumsy me

--- rolecommands/web.go
+++ rolecommands/web.go
@@ -425,6 +425,12 @@ func (p *Plugin) LoadServerHomeWidget(w http.ResponseWriter, r *http.Request) (w
 	templateData["SettingsPath"] = "/rolecommands/"
 
 	numCommands, err := models.RoleCommands(qm.Where("guild_id = ?", g.ID)).CountG(r.Context())
+	
+	if err != nil{
+		return templateData, err
+    	}
+	
+	numGroups, err := models.RoleGroups(qm.Where("guild_id = ?", g.ID)).CountG(r.Context())
 
 	if numCommands > 0 {
 		templateData["WidgetEnabled"] = true
@@ -432,7 +438,10 @@ func (p *Plugin) LoadServerHomeWidget(w http.ResponseWriter, r *http.Request) (w
 		templateData["WidgetDisabled"] = true
 	}
 
-	templateData["WidgetBody"] = template.HTML(fmt.Sprintf("Active RoleCommands: <code>%d</code>", numCommands))
+	templateData["WidgetBody"] = template.HTML(fmt.Sprintf(`<ul>
+		<li>Active RoleCommands: <code>%d</code></li>
+		<li>Active RoleGroups: <code>%d</code></li>
+		</ul>`, numCommands, numGroups))
 
 	return templateData, err
 }
