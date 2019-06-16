c73b87e15dede4ac726e759a8c7587ff699d6097
jonas747 <jonasr747@gmail.com>
2019-06-16 17:11:42 +0200

    timezonecompanion: slight tweaks

--- timezonecompanion/plugin_bot.go
+++ timezonecompanion/plugin_bot.go
@@ -27,52 +27,52 @@ func (p *Plugin) BotInit() {
 
 func (p *Plugin) AddCommands() {
 	commands.AddRootCommands(&commands.YAGCommand{
-		CmdCategory:  commands.CategoryTool,
-		Name:         "settimezone",
-		Aliases:      []string{"setz", "tzset"},
-		Description:  "Sets your timezone, used for various purposes such as auto conversion. Give it your country.",
+		CmdCategory: commands.CategoryTool,
+		Name:        "settimezone",
+		Aliases:     []string{"setz", "tzset"},
+		Description: "Sets your timezone, used for various purposes such as auto conversion. Give it your country.",
 		Arguments: []*dcmd.ArgDef{
 			&dcmd.ArgDef{Name: "Timezone", Type: dcmd.String},
 		},
 		ArgSwitches: []*dcmd.ArgDef{
-			&dcmd.ArgDef{Switch: "u", Name: "User"},
+			&dcmd.ArgDef{Switch: "u", Name: "Display current"},
 			&dcmd.ArgDef{Switch: "d", Name: "Delete TZ record"},
 		},
 		RunFunc: func(parsed *dcmd.Data) (interface{}, error) {
 
- 			localTZ := time.Now().Location()
-  			userZone, userOffset := time.Now().In(localTZ).Zone()
-  			getUserTZ := GetUserTimezone(parsed.Msg.Author.ID)
- 			tzState := "server's"
+			localTZ := time.Now().Location()
+			userZone, userOffset := time.Now().In(localTZ).Zone()
+			getUserTZ := GetUserTimezone(parsed.Msg.Author.ID)
+			tzState := "server's"
 
-  			if getUserTZ != nil {
-  				userZone, userOffset = time.Now().In(getUserTZ).Zone()
-  				localTZ = getUserTZ
-  				tzState = "registered to"
-  			} 
+			if getUserTZ != nil {
+				userZone, userOffset = time.Now().In(getUserTZ).Zone()
+				localTZ = getUserTZ
+				tzState = "registered to"
+			}
 
- 			humanizeOffset := fmt.Sprintf("%+d", userOffset/3600)
- 			if (userOffset%3600/60) != 0 {
- 				humanizeOffset += fmt.Sprintf(":%d", int(math.Abs(float64(userOffset%3600/60))))
- 			}
+			humanizeOffset := fmt.Sprintf("%+d", userOffset/3600)
+			if (userOffset % 3600 / 60) != 0 {
+				humanizeOffset += fmt.Sprintf(":%d", int(math.Abs(float64(userOffset%3600/60))))
+			}
 
- 			userTZ := fmt.Sprintf("Your current time zone is %s: `%s` %s (UTC%s)", tzState, localTZ, userZone, humanizeOffset)
- 
+			userTZ := fmt.Sprintf("Your current time zone is %s: `%s` %s (UTC%s)", tzState, localTZ, userZone, humanizeOffset)
+
 			if parsed.Switches["u"].Value != nil && parsed.Switches["u"].Value.(bool) {
 				return userTZ, nil
 			}
 
-			if parsed.Switches["d"].Value != nil && parsed.Switches["d"].Value.(bool) && getUserTZ !=nil {
- 				m := &models.UserTimezone{
+			if parsed.Switches["d"].Value != nil && parsed.Switches["d"].Value.(bool) && getUserTZ != nil {
+				m := &models.UserTimezone{
 					UserID:       parsed.Msg.Author.ID,
 					TimezoneName: localTZ.String(),
 				}
- 				_, err := m.DeleteG(parsed.Context())
+				_, err := m.DeleteG(parsed.Context())
 				if err != nil {
 					return nil, err
 				}
 				return "Deleted", nil
-			} else if parsed.Switches["d"].Value != nil && parsed.Switches["d"].Value.(bool){
+			} else if parsed.Switches["d"].Value != nil && parsed.Switches["d"].Value.(bool) {
 				return "You don't have a registered time zone", nil
 			}
 
