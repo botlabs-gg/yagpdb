67cac5e1ed46f3e6e4193766eb95110b6fcbf758
jonas747 <jonasr747@gmail.com>
2019-07-08 16:53:41 +0200

    automodv2: index foreign key because to speed it up

--- automod/db_schema.go
+++ automod/db_schema.go
@@ -92,4 +92,5 @@ CREATE TABLE IF NOT EXISTS automod_triggered_rules (
 );
 
 CREATE INDEX IF NOT EXISTS automod_triggered_rules_guild_idx ON automod_triggered_rules(guild_id);
+CREATE INDEX IF NOT EXISTS automod_triggered_rules_trigger_idx ON automod_triggered_rules(trigger_id);
 `
