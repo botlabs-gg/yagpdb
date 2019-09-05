1618e9c3a551f42e34955bde9ba70c99e3423315
buthed010203 <naguiar010203@gmail.com>
2019-09-05 11:55:01 -0400

    Fix a "bug"
    Removed the long garbled text as now multiple people have thought its a bug and changed it to "an error"

--- stdcommands/throw/throwthings.go
+++ stdcommands/throw/throwthings.go
@@ -51,7 +51,7 @@ var throwThings = []string{
 	"the biggest fattest vape",
 	"donald trump's wall",
 	"smash mouth - all star",
-	"H4sIABaqgVoA/1WPvW7FIAyFd57ibHmOTlWWrlVHenFIGrCvwCTi7a9Dq0rZ0Pn5OP6kqRBYULV4jlQqVJDkIPclDTvLCV0JpSWq8BxQBUEwuzcsLSU8JOdNM7FOFefqFfOUrbLxvnGELINzSkuBJ0UkvcyKpUg2XocYviC27tyMn1YVp2f2UDJ6t+5qEy7mQpQM6d5F1SP7nYbdONhqtWnOfdBxseTqx+34CzxverIFlxzk5JtRGsMXMd64M1ClMqK31P+/j9JvRvXdXhK+O930cYd9u9HAru0X+gIEKbNKeQEAAA==",
+	"an error",
 	"death",
 	"tide pods",
 	"a happy life with a good career and a nice family",
