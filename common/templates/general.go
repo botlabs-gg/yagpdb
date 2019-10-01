9b34a5e03191d1b27dea91517dcfb9004bfa2b91
caubert <37475633+mrbentarikau@users.noreply.github.com>
2019-10-01 11:03:17 +0300

    Update general.go

--- common/templates/general.go
+++ common/templates/general.go
@@ -282,14 +282,10 @@ func tmplFDiv(args ...interface{}) interface{} {
 	return sumF
 }
 
-func tmplSqrt(args ...interface{}) float64 {
-	if len(args) != 1 {
-		return math.Sqrt(-1)
-	}
-
-	switch args[0].(type) {
+func tmplSqrt(arg interface{}) float64 {
+	switch arg.(type) {
 	case int, int16, int32, int64, uint8, uint16, uint32, uint64, float32, float64:
-		return math.Sqrt(ToFloat64(args[0]))
+		return math.Sqrt(ToFloat64(arg))
 	default:
 		return math.Sqrt(-1)
 	}
