dc198bab19ca8cb6489878186f0e1df874ed1a6c
jonas747 <jonasr747@gmail.com>
2019-11-14 05:32:11 +0100

    serverstats: fix wrong column

--- serverstats/serverstats.go
+++ serverstats/serverstats.go
@@ -96,7 +96,7 @@ func RetrieveDailyStats(guildID int64) (*DailyStats, error) {
 	t := RoundHour(time.Now())
 	memberStatsRows, err := models.ServerStatsMemberPeriods(
 		models.ServerStatsMemberPeriodWhere.GuildID.EQ(guildID),
-		qm.Where("started > ?", time.Now().Add(time.Hour*-25))).AllG(context.Background())
+		qm.Where("created_at > ?", time.Now().Add(time.Hour*-25))).AllG(context.Background())
 
 	// Sum the stats
 	for i, v := range memberStatsRows {
