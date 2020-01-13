09a160808744d72fc55bfe6962efa498873d35c2
caubert <37475633+mrbentarikau@users.noreply.github.com>
2020-01-13 08:50:31 +0200

    changed mina to minage
    change the switch name, kept it all lowercase for the command, that's how it's currently  everywhere although minAge there would be more understandable as name
    
    another thin -i switch is quite pointless (maybe for historical reasons and this command is old), because -r as regex already has (?i) inbuilt for case sensitivity

--- moderation/commands.go
+++ moderation/commands.go
@@ -323,7 +323,7 @@ var ModerationCommands = []*commands.YAGCommand{
 		ArgSwitches: []*dcmd.ArgDef{
 			&dcmd.ArgDef{Switch: "r", Name: "Regex", Type: dcmd.String},
 			&dcmd.ArgDef{Switch: "ma", Default: time.Duration(0), Name: "Max age", Type: &commands.DurationArg{}},
-			&dcmd.ArgDef{Switch: "mina", Default: time.Duration(0), Name: "Min age", Type: &commands.DurationArg{}},
+			&dcmd.ArgDef{Switch: "minage", Default: time.Duration(0), Name: "Min age", Type: &commands.DurationArg{}},
 			&dcmd.ArgDef{Switch: "i", Name: "Regex case insensitive"},
 			&dcmd.ArgDef{Switch: "nopin", Name: "Ignore pinned messages"},
 		},
@@ -380,8 +380,8 @@ var ModerationCommands = []*commands.YAGCommand{
 			}
 
 			// Check if we have a min age
-			mina := parsed.Switches["mina"].Value.(time.Duration)
-			if mina != 0 {
+			minAge := parsed.Switches["minage"].Value.(time.Duration)
+			if minAge != 0 {
 				filtered = true
 			}
 			
@@ -404,7 +404,7 @@ var ModerationCommands = []*commands.YAGCommand{
 			// Wait a second so the client dosen't gltich out
 			time.Sleep(time.Second)
 
-			numDeleted, err := AdvancedDeleteMessages(parsed.Msg.ChannelID, userFilter, re, ma, mina, pe, num, limitFetch)
+			numDeleted, err := AdvancedDeleteMessages(parsed.Msg.ChannelID, userFilter, re, ma, minAge, pe, num, limitFetch)
 
 			return dcmd.NewTemporaryResponse(time.Second*5, fmt.Sprintf("Deleted %d message(s)! :')", numDeleted), true), err
 		},
