a2bb6f62393ea0aea213366992464f0888cda138
caubert <37475633+mrbentarikau@users.noreply.github.com>
2020-01-22 14:07:42 +0200

    typofix for cmdFuncMentionRole return (#601)

--- stdcommands/mentionrole/mentionrole.go
+++ stdcommands/mentionrole/mentionrole.go
@@ -78,7 +78,7 @@ func cmdFuncMentionRole(data *dcmd.Data) (interface{}, error) {
 	if ok, err := bot.AdminOrPerm(discordgo.PermissionManageRoles, data.Msg.Author.ID, data.CS.ID); err != nil {
 		return "Failed checking perms", err
 	} else if !ok {
-		return "You need manage server perms to use this command", nil
+		return "You need manage roles perms to use this command", nil
 	}
 
 	roleS := data.Args[0].Str()
