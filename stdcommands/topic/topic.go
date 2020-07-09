fedd74f963d27aeb046a30f1111717e3a8ebb246
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-09 15:41:39 -0400

    Create topic.go
    restored topic file

---
+++ stdcommands/topic/topic.go
@@ -0,0 +1,21 @@
+package topic
+
+
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/jonas747/dcmd"
+	"github.com/jonas747/yagpdb/commands"
+)
+
+var Command = &commands.YAGCommand{
+	CmdCategory: commands.CategoryFun,
+	Name:        "Topic",
+	Description: "Generates a conversation topic to help chat get moving.",
+	RunFunc: func(data *dcmd.Data) (interface{}, error) {
+		resp := ""
+		resp = fmt.Sprintf("Lets talk about **%s**", randomTopic())
+		return resp, nil
+	},
+}
