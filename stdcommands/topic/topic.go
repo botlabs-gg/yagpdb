71c80b3595afd8b406efc893d9d4b8b8c6d3c873
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-09 15:49:28 -0400

    Update topic.go

--- stdcommands/topic/topic.go
+++ stdcommands/topic/topic.go
@@ -1,21 +1,24 @@
 package topic
 
-
 import (
-	"fmt"
-	"math/rand"
-
+	"github.com/PuerkitoBio/goquery"
 	"github.com/jonas747/dcmd"
 	"github.com/jonas747/yagpdb/commands"
 )
 
 var Command = &commands.YAGCommand{
+	Cooldown:    5,
 	CmdCategory: commands.CategoryFun,
 	Name:        "Topic",
 	Description: "Generates a conversation topic to help chat get moving.",
+
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		resp := ""
-		resp = fmt.Sprintf("Lets talk about **%s**", randomTopic())
-		return resp, nil
+		doc, err := goquery.NewDocument("http://www.conversationstarters.com/generator.php")
+		if err != nil {
+			return nil, err
+		}
+
+		topic := doc.Find("#random").Text()
+		return topic, nil
 	},
 }
