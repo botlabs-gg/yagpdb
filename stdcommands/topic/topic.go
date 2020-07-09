31f6531631865c2618ce2f1eaec752c8ab7dbad8
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-09 15:01:26 -0400

    format change
    Small changes

--- stdcommands/topic/topic.go
+++ stdcommands/topic/topic.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"math/rand"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/jonas747/dcmd"
 	"github.com/jonas747/yagpdb/commands"
 )
@@ -14,7 +13,6 @@ var Command = &commands.YAGCommand{
 	CmdCategory: commands.CategoryFun,
 	Name:        "Topic",
 	Description: "Generates a conversation topic to help chat get moving.",
-
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
 		resp := ""
 		resp = fmt.Sprintf("Lets talk about **%s**", randomTopic())
