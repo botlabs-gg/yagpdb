032c27e2363a16d0929c0c10b5debe10ef496dc9
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-09 15:22:20 -0400

    commi
    commit

---
+++ stdcommands/topic/randomTopic.go
@@ -0,0 +1,15 @@
+package topic
+
+import (
+	"math/rand"
+)
+
+func randomTopic() string {
+	return topicList[rand.Intn(len(topicList))]
+}
+
+var topicList = []string{
+	"why cats can't decide to go inside or outside",
+	"what type of weather you're having",
+	"the global impact of fruit rollups",
+}
