d0760f9723ac0a42e0372fc613d10f0192a53127
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-09 14:41:51 -0400

    TopicChanges v1
    Submit Topic Changes

--- stdcommands/topic/topic.go
+++ stdcommands/topic/topic.go
@@ -1,24 +1,23 @@
 package topic
 
+
 import (
+	"fmt"
+	"math/rand"
+
 	"github.com/PuerkitoBio/goquery"
 	"github.com/jonas747/dcmd"
 	"github.com/jonas747/yagpdb/commands"
 )
 
 var Command = &commands.YAGCommand{
-	Cooldown:    5,
 	CmdCategory: commands.CategoryFun,
 	Name:        "Topic",
 	Description: "Generates a conversation topic to help chat get moving.",
 
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		doc, err := goquery.NewDocument("https://capitalizemytitle.com/random-topic-generator/")
-		if err != nil {
-			return nil, err
-		}
-
-		topic := doc.Find("#blog-ideas-output").Text()
-		return topic, nil
+		resp := ""
+		resp = fmt.Sprintf("Lets talk about **%s**", randomTopic())
+		return resp, nil
 	},
 }
