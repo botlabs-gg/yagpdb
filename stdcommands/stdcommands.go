59e8d941bf7f8215efd4057a73f4dddab983ab89
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-13 11:34:51 -0400

    sum log

--- stdcommands/stdcommands.go
+++ stdcommands/stdcommands.go
@@ -47,6 +47,7 @@ import (
 	"github.com/jonas747/yagpdb/stdcommands/wouldyourather"
 	"github.com/jonas747/yagpdb/stdcommands/xkcd"
 	"github.com/jonas747/yagpdb/stdcommands/yagstatus"
+	"github.com/jonas747/yagpdb/stdcommands/newtopic"
 )
 
 var (
