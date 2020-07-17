bc85ae8b8a7e7daaf5a7c6ba5109f654ab2ed000
jonas747 <jonasr747@gmail.com>
2020-07-18 00:05:49 +0200

    update to latest dshardorchestrator

--- cmd/shardorchestrator/main.go
+++ cmd/shardorchestrator/main.go
@@ -23,6 +23,7 @@ var (
 	confTotalShards             = config.RegisterOption("yagpdb.sharding.total_shards", "Total number shards", 0)
 	confActiveShards            = config.RegisterOption("yagpdb.sharding.active_shards", "Shards active on this hoste, ex: '1-10,25'", "")
 	confLargeBotShardingEnabled = config.RegisterOption("yagpdb.large_bot_sharding", "Set to enable large bot sharding (for 200k+ guilds)", false)
+	confBucketsPerNode          = config.RegisterOption("yagpdb.shard.buckets_per_node", "Number of buckets per node", 8)
 )
 
 func main() {
@@ -60,6 +61,7 @@ func main() {
 
 	if confLargeBotShardingEnabled.GetBool() {
 		orch.ShardBucketSize = 2
+		orch.BucketsPerNode = confBucketsPerNode.GetInt()
 	}
 
 	updateScript := "updateversion.sh"
