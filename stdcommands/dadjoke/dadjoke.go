7808503ad81282084f4302b2da17d4bef4ec430a
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-17 12:12:15 -0400

    Update dadjoke.go

--- stdcommands/dadjoke/dadjoke.go
+++ stdcommands/dadjoke/dadjoke.go
@@ -1,10 +1,19 @@
 package dadjoke
 
 import (
-	"github.com/PuerkitoBio/goquery"
-	"github.com/jonas747/dcmd"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"encoding/json"
 	"github.com/jonas747/yagpdb/commands"
 )
+
+type Joke struct {
+	ID string `json:"id"`
+	JOKE string `json:"joke"`
+	STATUS int `json:"status"`
+}
 
 var Command = &commands.YAGCommand{
 	Cooldown:    5,
@@ -13,12 +22,31 @@ var Command = &commands.YAGCommand{
 	Description: "Generates a dad joke for no reason other than annoying the staff.",
 
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		doc, err := goquery.NewDocument("https://icanhazdadjoke.com/")
+		req, err := http.NewRequest("GET", "https://icanhazdadjoke.com", nil)
 		if err != nil {
-			return nil, err
+			log.Fatalln(err)
 		}
-
-		topic := doc.Find("#Joke").Text()
-		return topic, nil
+	
+		req.Header.Set("Accept", "application/json")
+	
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	
+		defer resp.Body.Close()
+	
+		bytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		joke1 := Joke{}
+		jsonErr := json.Unmarshal(bytes,&joke1)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+		resp := fmt.Println(joke1.JOKE)
+		return resp , nil
 	},
 }
