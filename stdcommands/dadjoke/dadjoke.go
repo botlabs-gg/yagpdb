bf4d6786d37bd44db75582b2fce9371d6f899f05
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-20 13:40:56 -0400

    standardization
    correcting formating to match standards
    updated error handling

--- stdcommands/dadjoke/dadjoke.go
+++ stdcommands/dadjoke/dadjoke.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jonas747/yagpdb/commands"
 )
 
+//Create the struct that we will serialize the api respone into.
 type Joke struct {
 	ID string `json:"id"`
 	JOKE string `json:"joke"`
@@ -22,30 +23,33 @@ var Command = &commands.YAGCommand{
 	Description: "Generates a dad joke using the API from icanhazdadjoke.",
 
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
+		//Define the request and website we will navigate to. 
 		req, err := http.NewRequest("GET", "https://icanhazdadjoke.com", nil)
 		if err != nil {
-			log.Fatalln(err)
+			return nil, err
 		}
-	
+		//Set the headers that will be sent to the API to determine the response.
 		req.Header.Set("Accept", "application/json")
 	
 		client := &http.Client{}
 		apiResp, err := client.Do(req)
 		if err != nil {
-			log.Fatalln(err)
+			return nil, err
 		}
-	
+		//Once the rest of the function is done close our connection the API.
 		defer apiResp.Body.Close()
-	
+		//Read the API response.
 		bytes, err := ioutil.ReadAll(apiResp.Body)
 		if err != nil {
-			log.Fatalln(err)
+			return nil, err
 		}
+		//Create our struct and unmarshal the content into it.
 		joke1 := Joke{}
 		jsonErr := json.Unmarshal(bytes,&joke1)
 		if jsonErr != nil {
-			log.Fatal(jsonErr)
+			return nil, jsonErr
 		}
+		//Return the joke - the other pieces are unneeded and ignored. 
 		resp := joke1.JOKE
 		return resp  , nil
 	},
