0e764a147023fa8efaf2bbf0646c43e30b38c367
JordanTheITGuy <32438363+JordanTheITGuy@users.noreply.github.com>
2020-07-20 14:11:12 -0400

    fixed a case of the dumb.
    Fixed - but this time with more feeling.

--- stdcommands/dadjoke/dadjoke.go
+++ stdcommands/dadjoke/dadjoke.go
@@ -25,7 +25,7 @@ var Command = &commands.YAGCommand{
 		//Define the request and website we will navigate to. 
 		req, err := http.NewRequest("GET", "https://icanhazdadjoke.com", nil)
 		if err != nil {
-			return err , nil
+			return nil , err
 		}
 		//Set the headers that will be sent to the API to determine the response.
 		req.Header.Set("Accept", "application/json")
@@ -33,20 +33,20 @@ var Command = &commands.YAGCommand{
 		client := &http.Client{}
 		apiResp, err := client.Do(req)
 		if err != nil {
-			return err, nil
+			return nil , err
 		}
 		//Once the rest of the function is done close our connection the API.
 		defer apiResp.Body.Close()
 		//Read the API response.
 		bytes, err := ioutil.ReadAll(apiResp.Body)
 		if err != nil {
-			return err , nil
+			return nil , err
 		}
 		//Create our struct and unmarshal the content into it.
 		joke1 := Joke{}
 		jsonErr := json.Unmarshal(bytes,&joke1)
 		if jsonErr != nil {
-			return jsonErr , nil
+			return nil , jsonErr
 		}
 		//Return the joke - the other pieces are unneeded and ignored. 
 		resp := joke1.JOKE
