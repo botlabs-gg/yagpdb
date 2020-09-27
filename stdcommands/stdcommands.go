617c3e762db07e72ee53813e6c8217454279ca96
caubert@gmail.com <caubert@gmail.com>
2020-09-27 18:51:14 +0300

    howlongtobeat

--- stdcommands/stdcommands.go
+++ stdcommands/stdcommands.go
@@ -20,6 +20,7 @@ import (
 	"github.com/jonas747/yagpdb/stdcommands/dogfact"
 	"github.com/jonas747/yagpdb/stdcommands/findserver"
 	"github.com/jonas747/yagpdb/stdcommands/globalrl"
+	"github.com/jonas747/yagpdb/stdcommands/howlongtobeat"
 	"github.com/jonas747/yagpdb/stdcommands/info"
 	"github.com/jonas747/yagpdb/stdcommands/invite"
 	"github.com/jonas747/yagpdb/stdcommands/leaveserver"
@@ -92,6 +93,7 @@ func (p *Plugin) AddCommands() {
 		viewperms.Command,
 		topgames.Command,
 		xkcd.Command,
+		howlongtobeat.Command,
 
 		// Maintenance
 		stateinfo.Command,
