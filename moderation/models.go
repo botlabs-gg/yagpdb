738a50e440aa3938e73ec729e465299b3d372de7
Pedro Pessoa <pedro_santos_40@hotmail.com>
2020-12-23 13:16:34 -0300

    make logging kicks not made through yag optional

--- moderation/models.go
+++ moderation/models.go
@@ -24,11 +24,11 @@ type Config struct {
 	KickMessage          string `valid:"template,5000"`
 
 	// Ban
-	BanEnabled        	bool
-	BanCmdRoles       	pq.Int64Array `gorm:"type:bigint[]" valid:"role,true"`
-	BanReasonOptional 	bool
-	BanMessage        	string `valid:"template,5000"`
-	DefaultBanDeleteDays    sql.NullInt64 `gorm:"default:1" valid:"0,7"`
+	BanEnabled           bool
+	BanCmdRoles          pq.Int64Array `gorm:"type:bigint[]" valid:"role,true"`
+	BanReasonOptional    bool
+	BanMessage           string        `valid:"template,5000"`
+	DefaultBanDeleteDays sql.NullInt64 `gorm:"default:1" valid:"0,7"`
 
 	// Mute/unmute
 	MuteEnabled             bool
@@ -58,6 +58,7 @@ type Config struct {
 	ReportChannel string `valid:"channel,true"`
 	LogUnbans     bool
 	LogBans       bool
+	LogKicks      bool `gorm:"default:true"`
 
 	GiveRoleCmdEnabled bool
 	GiveRoleCmdModlog  bool
