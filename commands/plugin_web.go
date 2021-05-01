e202003d7bcebc6ae20a95d68d7fc4d87706be3d
Luca Z <l-zeuch@email.de>
2021-05-02 01:27:28 +0200

    style(prefix): use the correct function you noob
    Co-authored-by: Joe <56809242+jo3-l@users.noreply.github.com>

--- commands/plugin_web.go
+++ commands/plugin_web.go
@@ -185,7 +185,7 @@ func HandleCommands(w http.ResponseWriter, r *http.Request) (web.TemplateData, e
 func HandlePostCommands(w http.ResponseWriter, r *http.Request) (web.TemplateData, error) {
 	ctx := r.Context()
 	activeGuild, templateData := web.GetBaseCPContextData(ctx)
-	newPrefix := strings.TrimLeft(r.FormValue("Prefix"))
+	newPrefix := strings.TrimLeftFunc(r.FormValue("Prefix"), unicode.IsSpace)
 	if len(newPrefix) < 1 || len(newPrefix) > 100 {
 		return templateData, web.NewPublicError("Prefix is smaller than 1 or larger than 100 characters")
 	}
