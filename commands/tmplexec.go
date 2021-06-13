085d92b6c0ecd1edab0e9268b070cba06f3b31ea
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-06-13 13:51:38 -0700

    commands: handle user value type in exec

--- commands/tmplexec.go
+++ commands/tmplexec.go
@@ -163,6 +163,9 @@ func execCmd(tmplCtx *templates.Context, dryRun bool, m *discordgo.MessageCreate
 		case *discordgo.User:
 			cmdLine += "<@" + strconv.FormatInt(t.ID, 10) + ">"
 			fakeMsg.Mentions = append(fakeMsg.Mentions, t)
+		case discordgo.User:
+			cmdLine += "<@" + strconv.FormatInt(t.ID, 10) + ">"
+			fakeMsg.Mentions = append(fakeMsg.Mentions, &t)
 		case []string:
 			for i, str := range t {
 				if i != 0 {
