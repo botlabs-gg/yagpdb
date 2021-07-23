eb7e54dc709bd2e7cf10348a47e35200fd7c981f
MatiasMFM2001 <46432351+MatiasMFM2001@users.noreply.github.com>
2021-07-23 17:40:37 -0300

    remindme: Add codeblock to command response
    This should make `Reminders` command easier to find, inside the response message. Also, I've converted it to *Title Case*
    I didn't test these changes on a selfhost, but they should work
    
    Should I add Markdown to any other parts? For example, something like this:
    > Set a reminder in **2 hours and 16 minutes** from now (`23 Jul 21 21:59 UTC`)

--- reminders/plugin_bot.go
+++ reminders/plugin_bot.go
@@ -67,7 +67,7 @@ var cmds = []*commands.YAGCommand{
 				return nil, err
 			}
 
-			return "Set a reminder in " + durString + " from now (" + tStr + ")\nView reminders with the reminders command", nil
+			return "Set a reminder in " + durString + " from now (" + tStr + ")\nView reminders with the `Reminders` command", nil
 		},
 	},
 	{
