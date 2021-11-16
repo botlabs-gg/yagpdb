c90cb2fc852493dffa29b2a0e62a9bc28e10156e
LemmeCry <67655446+LemmeCry@users.noreply.github.com>
2021-11-16 10:22:25 +0530

    add getPinCount template

--- common/templates/context_funcs.go
+++ common/templates/context_funcs.go
@@ -1157,6 +1157,24 @@ func (c *Context) tmplGetChannelOrThread(channel interface{}) (*CtxChannel, erro
 	return CtxChannelFromCS(cstate), nil
 }
 
+func (c *Context) tmplGetChannelPinCount(channel interface{}) (int, error) {
+	if c.IncreaseCheckCallCounterPremium("count_pins", 2, 4) {
+		return 0, ErrTooManyCalls
+	}
+
+	cID := c.ChannelArgNoDM(channel)
+	if cID == 0 {
+		return 0, errors.New("unknown channel")
+	}
+
+	msg, err := common.BotSession.ChannelMessagesPinned(cID)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(msg), nil
+}
+
 func (c *Context) tmplAddReactions(values ...reflect.Value) (reflect.Value, error) {
 	f := func(args []reflect.Value) (reflect.Value, error) {
 		if c.Msg == nil {
