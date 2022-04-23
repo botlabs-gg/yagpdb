288f34fe0aee2f43fb35c3f278dd8c31dc485176
savage4618 <savage4618@users.noreply.github.com>
2022-04-23 17:18:26 -0400

    change custom user agent string for icanhazdadjoke api

--- stdcommands/dadjoke/dadjoke.go
+++ stdcommands/dadjoke/dadjoke.go
@@ -29,7 +29,7 @@ var Command = &commands.YAGCommand{
 			return nil , err
 		}
 		//Set the headers that will be sent to the API to determine the response.
-		req.Header.Set("Accept", "application/json", "User-Agent", "Discord bot (https://github.com/savage4618/yagpdb)")
+		req.Header.Set("Accept", "application/json", "User-Agent", "YAGPDB.xyz (https://github.com/botlabs-gg/yagpdb)")
 
 		client := &http.Client{}
 		apiResp, err := client.Do(req)
