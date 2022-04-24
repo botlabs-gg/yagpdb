320f55db4e0e5081edd7474d8c30100c3f1e0795
savage4618 <savage4618@users.noreply.github.com>
2022-04-23 21:32:39 -0400

    the last changes broke everything. This commit is reverting back to 89bc5d7

--- stdcommands/dadjoke/dadjoke.go
+++ stdcommands/dadjoke/dadjoke.go
@@ -11,8 +11,8 @@ import (
 //Create the struct that we will serialize the api respone into.
 type Joke struct {
 	ID string `json:"id"`
-	Joke string `json:"joke"`
-	Status int `json:"status"`
+	JOKE string `json:"joke"`
+	STATUS int `json:"status"`
 }
 
 var Command = &commands.YAGCommand{
@@ -32,24 +32,26 @@ var Command = &commands.YAGCommand{
 		req.Header.Set("Accept", "application/json")
 		req.Header.Add("User-Agent", "YAGPDB.xyz (https://github.com/botlabs-gg/yagpdb)")
 
-		apiResp, err := http.DefaultClient.Do(req)
+		client := &http.Client{}
+		apiResp, err := client.Do(req)
 		if err != nil {
 			return nil , err
 		}
 		//Once the rest of the function is done close our connection the API.
 		defer apiResp.Body.Close()
 		//Read the API response.
-		bytes, err := io.ReadAll(apiResp.Body)
+		bytes, err := ioutil.ReadAll(apiResp.Body)
 		if err != nil {
 			return nil , err
 		}
 		//Create our struct and unmarshal the content into it.
-		var joke := Joke{}
-		err := json.Unmarshal(bytes,&joke1)
-		if err != nil {
-			return nil , err
+		joke1 := Joke{}
+		jsonErr := json.Unmarshal(bytes,&joke1)
+		if jsonErr != nil {
+			return nil , jsonErr
 		}
 		//Return the joke - the other pieces are unneeded and ignored. 
-		return joke.Joke , nil
+		resp := joke1.JOKE
+		return resp , nil
 	},
 }
