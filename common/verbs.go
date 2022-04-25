54c175c3f6a818f39a97be485338b8e8a5fac2b8
User <65233836+User9684@users.noreply.github.com>
2022-04-25 18:19:29 -0500

    Create verbs.go

---
+++ common/verbs.go
@@ -0,0 +1,21 @@
+// this package provides core functinality to yagpdb, important security stuff here
+package common
+
+var Verbs = []string{
+	"accept",
+	"accuse",
+	"achieve",
+	"acquire",
+	"acknowledge",
+	"add",
+	"admire",
+	"admit",
+	"advise",
+	"afford",
+	"agree",
+	"allow",
+	"bake",
+	"be",
+	"bear",
+	"beg",
+},
