64c26532705f0ac5d9b9f1605a590823591d0f56
savage4618 <savage4618@users.noreply.github.com>
2022-04-27 19:48:30 -0400

    actually read through Joe's notes and actually fixed the file (i wasn't drinking this time)

--- stdcommands/dadjoke/dadjoke.go
+++ stdcommands/dadjoke/dadjoke.go
@@ -1,57 +1,52 @@
 package dadjoke
 
 import (
-	"io/ioutil"
-	"net/http"
 	"encoding/json"
-	"github.com/jonas747/dcmd/v4"
+	"io"
+	"net/http"
+
 	"github.com/botlabs-gg/yagpdb/commands"
+	"github.com/jonas747/dcmd/v4"
 )
 
-//Create the struct that we will serialize the api respone into.
 type Joke struct {
-	ID string `json:"id"`
-	JOKE string `json:"joke"`
-	STATUS int `json:"status"`
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
 }
 
 var Command = &commands.YAGCommand{
-	Cooldown:    5,
-	CmdCategory: commands.CategoryFun,
-	Name:        "DadJoke",
-	Description: "Generates a dad joke using the API from icanhazdadjoke.",
-	DefaultEnabled: 		true,
-	SlashCommandEnabled: 	true,
+	Cooldown:            5,
+	CmdCategory:         commands.CategoryFun,
+	Name:                "DadJoke",
+	Description:         "Generates a dad joke using the API from icanhazdadjoke.",
+	DefaultEnabled:      true,
+	SlashCommandEnabled: true,
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		//Define the request and website we will navigate to. 
 		req, err := http.NewRequest("GET", "https://icanhazdadjoke.com", nil)
 		if err != nil {
-			return nil , err
+			return nil, err
 		}
 		//Set the headers that will be sent to the API to determine the response.
 		req.Header.Set("Accept", "application/json")
 		req.Header.Add("User-Agent", "YAGPDB.xyz (https://github.com/botlabs-gg/yagpdb)")
 
-		client := &http.Client{}
-		apiResp, err := client.Do(req)
+		apiResp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			return nil , err
+			return nil, err
 		}
-		//Once the rest of the function is done close our connection the API.
 		defer apiResp.Body.Close()
-		//Read the API response.
-		bytes, err := ioutil.ReadAll(apiResp.Body)
+		bytes, err := io.ReadAll(apiResp.Body)
 		if err != nil {
-			return nil , err
+			return nil, err
 		}
-		//Create our struct and unmarshal the content into it.
-		joke1 := Joke{}
-		jsonErr := json.Unmarshal(bytes,&joke1)
-		if jsonErr != nil {
-			return nil , jsonErr
+		var joke Joke
+		err = json.Unmarshal(bytes, &joke)
+		if err != nil {
+			return nil, err
 		}
-		//Return the joke - the other pieces are unneeded and ignored. 
-		resp := joke1.JOKE
-		return resp , nil
+		//Return the joke - the other pieces are unneeded and ignored.
+		resp := joke.Joke
+		return resp, nil
 	},
 }
