7c4aa44e3c820f993449403d4bc92e602e970d33
lemm-e <67655446+lemm-e@users.noreply.github.com>
2022-05-19 03:15:55 +0530

    add new permissions and show them in viewperms command

--- common/util.go
+++ common/util.go
@@ -374,6 +374,9 @@ func HumanizePermissions(perms int64) (res []string) {
 	if perms&discordgo.PermissionManageServer == discordgo.PermissionManageServer {
 		res = append(res, "ManageServer")
 	}
+	if perms&discordgo.PermissionViewGuildInsights == discordgo.PermissionViewGuildInsights {
+		res = append(res, "ViewGuildInsights")
+	}
 
 	if perms&discordgo.PermissionReadMessages == discordgo.PermissionReadMessages {
 		res = append(res, "ReadMessages")
@@ -402,7 +405,15 @@ func HumanizePermissions(perms int64) (res []string) {
 	if perms&discordgo.PermissionUseExternalEmojis == discordgo.PermissionUseExternalEmojis {
 		res = append(res, "UseExternalEmojis")
 	}
-
+	if perms&discordgo.PermissionUseExternalStickers == discordgo.PermissionUseExternalStickers {
+		res = append(res, "UseExternalStickers")
+	}
+	if perms&discordgo.PermissionUseApplicationCommands == discordgo.PermissionUseApplicationCommands {
+		res = append(res, "UseApplicationCommands")
+	}
+	if perms&discordgo.PermissionUseEmbeddedActivities == discordgo.PermissionUseEmbeddedActivities {
+		res = append(res, "UseEmbeddedActivities")
+	}
 	// Constants for the different bit offsets of voice permissions
 	if perms&discordgo.PermissionVoiceConnect == discordgo.PermissionVoiceConnect {
 		res = append(res, "VoiceConnect")
@@ -422,7 +433,15 @@ func HumanizePermissions(perms int64) (res []string) {
 	if perms&discordgo.PermissionVoiceUseVAD == discordgo.PermissionVoiceUseVAD {
 		res = append(res, "VoiceUseVAD")
 	}
-
+	if perms&discordgo.PermissionPrioritySpeaker == discordgo.PermissionPrioritySpeaker {
+		res = append(res, "PrioritySpeaker")
+	}
+	if perms&discordgo.PermissionRequestToSpeak == discordgo.PermissionRequestToSpeak {
+		res = append(res, "RequestToSpeak")
+	}
+	if perms&discordgo.PermissionStream == discordgo.PermissionStream {
+		res = append(res, "Stream")
+	}
 	// Constants for general management.
 	if perms&discordgo.PermissionChangeNickname == discordgo.PermissionChangeNickname {
 		res = append(res, "ChangeNickname")
@@ -436,12 +455,15 @@ func HumanizePermissions(perms int64) (res []string) {
 	if perms&discordgo.PermissionManageWebhooks == discordgo.PermissionManageWebhooks {
 		res = append(res, "ManageWebhooks")
 	}
-	if perms&discordgo.PermissionManageEmojis == discordgo.PermissionManageEmojis {
-		res = append(res, "ManageEmojis")
+	if perms&discordgo.PermissionManageEmojisAndStickers == discordgo.PermissionManageEmojisAndStickers {
+		res = append(res, "ManageEmojisAndStickers")
 	}
 
 	if perms&discordgo.PermissionCreateInstantInvite == discordgo.PermissionCreateInstantInvite {
 		res = append(res, "CreateInstantInvite")
+	}
+	if perms&discordgo.PermissionModerateMembers == discordgo.PermissionModerateMembers {
+		res = append(res, "ModerateMembers")
 	}
 	if perms&discordgo.PermissionKickMembers == discordgo.PermissionKickMembers {
 		res = append(res, "KickMembers")
@@ -452,13 +474,29 @@ func HumanizePermissions(perms int64) (res []string) {
 	if perms&discordgo.PermissionManageChannels == discordgo.PermissionManageChannels {
 		res = append(res, "ManageChannels")
 	}
+	if perms&discordgo.PermissionManageEvents == discordgo.PermissionManageEvents {
+		res = append(res, "ManageEvents")
+	}
+
+	if perms&discordgo.PermissionManageThreads == discordgo.PermissionManageThreads {
+		res = append(res, "ManageThreads")
+	}
+	if perms&discordgo.PermissionUsePublicThreads == discordgo.PermissionUsePublicThreads {
+		res = append(res, "UsePublicThreads")
+	}
+	if perms&discordgo.PermissionUsePrivateThreads == discordgo.PermissionUsePrivateThreads {
+		res = append(res, "UsePrivateThreads")
+	}
+	if perms&discordgo.PermissionSendMessagesInThreads == discordgo.PermissionSendMessagesInThreads {
+		res = append(res, "SendMessagesInThreads")
+	}
+
 	if perms&discordgo.PermissionAddReactions == discordgo.PermissionAddReactions {
 		res = append(res, "AddReactions")
 	}
 	if perms&discordgo.PermissionViewAuditLogs == discordgo.PermissionViewAuditLogs {
 		res = append(res, "ViewAuditLogs")
 	}
-
 	return
 }
 
