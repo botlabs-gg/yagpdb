7a687d599fd0cf00789ac36780cbe8c57e9db12d
caubert <caubert@gmail.com>
2022-05-19 15:50:21 +0300

    Update context.go
    .BotUser to context data

--- common/templates/context.go
+++ common/templates/context.go
@@ -228,6 +228,7 @@ func (c *Context) setupBaseData() {
 	}
 
 	if c.MS != nil {
+		c.Data["BotUser"] = common.BotUser
 		c.Data["Member"] = c.MS.DgoMember()
 		c.Data["User"] = &c.MS.User
 		c.Data["user"] = c.Data["User"]
