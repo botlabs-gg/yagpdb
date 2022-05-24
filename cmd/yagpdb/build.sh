67598477ac5149967e8c998e3c42094e5fd2f504
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-05-24 20:42:36 +0530

    add v2 to the build yagpdb package path in build.sh

--- cmd/yagpdb/build.sh
+++ cmd/yagpdb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION=$(git describe --tags)
 echo Building version $VERSION
-go build -ldflags "-X github.com/botlabs-gg/yagpdb/common.VERSION=${VERSION}"
+go build -ldflags "-X github.com/botlabs-gg/yagpdb/v2/common.VERSION=${VERSION}"
