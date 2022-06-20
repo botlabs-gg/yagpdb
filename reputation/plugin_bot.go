a0dc8d3a476ed46f87a035456ffac70c1c10bfab
Ankur P <98815360+ankurp-bst@users.noreply.github.com>
2022-06-20 18:44:25 +0530

    Add whitelist and blacklist channels for rep thanks detection (#1269)
    * Add whitelist and blacklist channels for rep thanks detection
    
    * Improve conditions to return if thanks detection is allowed in a channel

--- reputation/plugin_bot.go
+++ reputation/plugin_bot.go
@@ -53,16 +53,20 @@ func handleMessageCreate(evt *eventsystem.EventData) {
 		return
 	}
 
+	if !evt.HasFeatureFlag(featureFlagThanksEnabled) {
+		return
+	}
+
 	if !thanksRegex.MatchString(msg.Content) {
+		return
+	}
+
+	if !isThanksDetectionAllowedInChannel(conf, msg.ChannelID) {
 		return
 	}
 
 	who := msg.Mentions[0]
 	if who.ID == msg.Author.ID {
-		return
-	}
-
-	if !evt.HasFeatureFlag(featureFlagThanksEnabled) {
 		return
 	}
 
@@ -512,4 +516,15 @@ func userPresentInRepLog(userID int64, guildID int64, parsed *dcmd.Data) (found 
 		return false, nil
 	}
 	return true, nil
+}
+
+// Checks if the thanks detection is allowed to be run in the given channel
+func isThanksDetectionAllowedInChannel(config *models.ReputationConfig, channelID int64) bool {
+	if len(config.WhitelistedThanksChannels) > 0 {
+		return common.ContainsInt64Slice(config.WhitelistedThanksChannels, channelID)
+	}
+	if len(config.BlacklistedThanksChannels) > 0 {
+		return !common.ContainsInt64Slice(config.BlacklistedThanksChannels, channelID)
+	}
+	return true
 }
--- reputation/plugin_web.go
+++ reputation/plugin_web.go
@@ -27,32 +27,36 @@ var PageHTMLLeaderboard string
 var PageHTMLSettings string
 
 type PostConfigForm struct {
-	Enabled                 bool
-	EnableThanksDetection   bool
-	PointsName              string  `valid:",50"`
-	Cooldown                int     `valid:"0,86401"` // One day
-	MaxGiveAmount           int64   `valid:"1,"`
-	MaxRemoveAmount         int64   `valid:"1,"`
-	RequiredGiveRoles       []int64 `valid:"role,true"`
-	RequiredReceiveRoles    []int64 `valid:"role,true"`
-	BlacklistedGiveRoles    []int64 `valid:"role,true"`
-	BlacklistedReceiveRoles []int64 `valid:"role,true"`
-	AdminRoles              []int64 `valid:"role,true"`
+	Enabled                   bool
+	EnableThanksDetection     bool
+	PointsName                string  `valid:",50"`
+	Cooldown                  int     `valid:"0,86401"` // One day
+	MaxGiveAmount             int64   `valid:"1,"`
+	MaxRemoveAmount           int64   `valid:"1,"`
+	RequiredGiveRoles         []int64 `valid:"role,true"`
+	RequiredReceiveRoles      []int64 `valid:"role,true"`
+	BlacklistedGiveRoles      []int64 `valid:"role,true"`
+	BlacklistedReceiveRoles   []int64 `valid:"role,true"`
+	AdminRoles                []int64 `valid:"role,true"`
+	WhitelistedThanksChannels []int64 `valid:"channel,true"`
+	BlacklistedThanksChannels []int64 `valid:"channel,true"`
 }
 
 func (p PostConfigForm) RepConfig() *models.ReputationConfig {
 	return &models.ReputationConfig{
-		PointsName:              p.PointsName,
-		Enabled:                 p.Enabled,
-		Cooldown:                p.Cooldown,
-		MaxGiveAmount:           p.MaxGiveAmount,
-		MaxRemoveAmount:         p.MaxRemoveAmount,
-		RequiredGiveRoles:       p.RequiredGiveRoles,
-		RequiredReceiveRoles:    p.RequiredReceiveRoles,
-		BlacklistedGiveRoles:    p.BlacklistedGiveRoles,
-		BlacklistedReceiveRoles: p.BlacklistedReceiveRoles,
-		AdminRoles:              p.AdminRoles,
-		DisableThanksDetection:  !p.EnableThanksDetection,
+		PointsName:                p.PointsName,
+		Enabled:                   p.Enabled,
+		Cooldown:                  p.Cooldown,
+		MaxGiveAmount:             p.MaxGiveAmount,
+		MaxRemoveAmount:           p.MaxRemoveAmount,
+		RequiredGiveRoles:         p.RequiredGiveRoles,
+		RequiredReceiveRoles:      p.RequiredReceiveRoles,
+		BlacklistedGiveRoles:      p.BlacklistedGiveRoles,
+		BlacklistedReceiveRoles:   p.BlacklistedReceiveRoles,
+		AdminRoles:                p.AdminRoles,
+		DisableThanksDetection:    !p.EnableThanksDetection,
+		WhitelistedThanksChannels: p.WhitelistedThanksChannels,
+		BlacklistedThanksChannels: p.BlacklistedThanksChannels,
 	}
 }
 
@@ -123,6 +127,8 @@ func HandlePostReputation(w http.ResponseWriter, r *http.Request) (templateData 
 		"blacklisted_receive_roles",
 		"admin_roles",
 		"disable_thanks_detection",
+		"whitelisted_thanks_channels",
+		"blacklisted_thanks_channels",
 	), boil.Infer())
 
 	if err == nil {
