93de899c28aed42f18fd5413da14ff3104a7649d
shadow21AR <iamsaif.engg@gmail.com>
2022-09-17 17:55:32 +0530

    Updated again

--- stdcommands/inspire/inspire.go
+++ stdcommands/inspire/inspire.go
@@ -7,9 +7,11 @@ import (
 	"math/rand"
 	"net/http"
 	"regexp"
+	"time"
 
 	"github.com/botlabs-gg/yagpdb/v2/bot/paginatedmessages"
 	"github.com/botlabs-gg/yagpdb/v2/commands"
+	"github.com/botlabs-gg/yagpdb/v2/common"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
 	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
 )
@@ -26,127 +28,117 @@ var Command = &commands.YAGCommand{
 	Arguments: []*dcmd.ArgDef{
 		{Name: "Mindfulness", Type: &dcmd.IntArg{Min: 1, Max: 100}},
 	},
-
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		query := "https://inspirobot.me/api?generate=true"
-
 		if data.Args[0].Str() != "" {
-			//Generate Unique Session ID
-			ID, err := inspireFromAPI("https://inspirobot.me/api?getSessionID=1")
+			wonkyErr := "InspireAPI wonky... ducks are sad : /"
+			result, err := inspireFromAPI(true)
 			if err != nil {
-				return nil, err
-			}
-			//Use Unique Session ID for query
-			query = fmt.Sprint("https://inspirobot.me/api?generateFlow=1&sessionID=", ID)
-			result, err := inspireFromAPIMf(query)
-			if err != nil {
-				return nil, err
+				return wonkyErr, err
 			}
 			inspireArray := []string{}
-			inspireArray = arrayMaker(inspireArray, result.Data)
-			//Pagination decided max pages by user input API gives only 3 at a time :/
+			inspireArray = arrayMaker(inspireArray, result)
 			_, err = paginatedmessages.CreatePaginatedMessage(
 				data.GuildData.GS.ID, data.ChannelID, 1, data.Args[0].Int(), func(p *paginatedmessages.PaginatedMessage, page int) (*discordgo.MessageEmbed, error) {
 					if page-1 == len(inspireArray) {
-						result, err := inspireFromAPIMf(query)
+						result, err := inspireFromAPI(true)
 						if err != nil {
 							return nil, err
 						}
-						inspireArray = arrayMaker(inspireArray, result.Data)
+						inspireArray = arrayMaker(inspireArray, result)
 					}
-					return createInspireEmbed(inspireArray[page-1]), nil
+					return createInspireEmbed(inspireArray[page-1], true), nil
 				})
 			if err != nil {
-				return nil, nil
+				return nil, err
 			}
 			return "", nil
-		} else {
-			//Normal Image Inspire Output
-			inspData, err := inspireFromAPI(query)
-			if err != nil {
-				return fmt.Sprintf("%s\nInspiroBot wonky... sad times :/", err), err
-			}
-			embed := &discordgo.MessageEmbed{
-				Description: "Here's an inspirational quote:",
-				Color:       int(rand.Int63n(0xffffff)),
-				Image: &discordgo.MessageEmbedImage{
-					URL: inspData,
-				},
-			}
-			return embed, nil
 		}
+		//Normal Image Inspire Output
+		inspData, err := inspireFromAPI(false)
+		if err != nil {
+			return fmt.Sprintf("%s\nInspiroBot wonky... sad times :/", err), err
+		}
+		embed := createInspireEmbed(inspData, false)
+		return embed, nil
 	},
 }
 
-func inspireFromAPI(query string) (string, error) {
-	wonkyErr := "InspireAPI wonky... ducks are sad : /"
+func inspireFromAPI(mindfulnessMode bool) (string, error) {
+	query := "https://inspirobot.me/api?generate=true"
+	if mindfulnessMode {
+		query = fmt.Sprintf("https://inspirobot.me/api?generateFlow=1&sessionID=%d", time.Now().UTC().Unix())
+	}
+
 	req, err := http.NewRequest("GET", query, nil)
 	if err != nil {
-		return "wonkyErr", err
+		return "", err
 	}
+
 	req.Header.Set("User-Agent", "curl/7.83.1")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return wonkyErr, err
+		return "", err
 	}
+
 	if resp.StatusCode != 200 {
 		return "", commands.NewPublicError("HTTP err: ", resp.StatusCode)
 	}
+
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return wonkyErr, err
+		return "", err
 	}
+	if mindfulnessMode {
+		var mindful MindfulnessMode
+
+		err := json.Unmarshal([]byte(body), &mindful)
+		if err != nil {
+			return "", err
+		}
+
+		mindfulness := mindful.Data[1].Text
+		if len(mindfulness) > 4000 {
+			mindfulness = common.CutStringShort(mindfulness, 4000)
+		}
+
+		return mindfulness, nil
+	}
+
 	return string(body), nil
 }
-func inspireFromAPIMf(query string) (*RawObj, error) {
-	req, err := http.NewRequest("GET", query, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("User-Agent", "curl/7.83.1")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	rawObj := &RawObj{}
-	err = json.Unmarshal([]byte(body), &rawObj)
-	if err != nil {
-		return nil, err
-	}
-	return rawObj, nil
-}
-func createInspireEmbed(data string) *discordgo.MessageEmbed {
-	embed := &discordgo.MessageEmbed{
-		Title:       "Here's an inspirational quote (Mindfulness Mode):",
-		Color:       int(11413503),
-		Description: "```\n" + data + "```",
+
+func createInspireEmbed(data string, mindfulness bool) *discordgo.MessageEmbed {
+	embed := &discordgo.MessageEmbed{}
+
+	if mindfulness {
+		embed.Title = "Here's an inspirational quote (Mindfulness Mode):"
+		embed.Description = "```\n" + data + "```"
+		embed.Color = int(11413503)
+	} else {
+		embed.Color = int(rand.Int63n(0xffffff))
+		embed.Description = "Here's an inspirational quote:"
+		embed.Image = &discordgo.MessageEmbedImage{
+			URL: data,
+		}
 	}
 	return embed
 }
-func arrayMaker(list []string, data []*InspireData) []string {
-	for _, i := range data {
-		if i.Text != "" {
-			re := regexp.MustCompile(`\[pause \d+\]`)
-			list = append(list, re.ReplaceAllString(i.Text, ""))
-		}
+func arrayMaker(list []string, data string) []string {
+	if data != "" {
+		re := regexp.MustCompile(`\[pause \d+\]`)
+		list = append(list, re.ReplaceAllString(data, ""))
 	}
 	return list
 }
 
-type RawObj struct {
-	Data []*InspireData `json:"data"`
-	Mp3  string         `json:"mp3"`
-}
-type InspireData struct {
-	Duration *float64 `json:"duration,omitempty"`
-	Image    *string  `json:"image,omitempty"`
-	Type     *string  `json:"type"`
-	Time     *float64 `json:"time"`
-	Text     string   `json:"text,omitempty"`
+type MindfulnessMode struct {
+	Data []struct {
+		Duration *float64 `json:"duration,omitempty"`
+		Image    *string  `json:"image,omitempty"`
+		Type     *string  `json:"type"`
+		Time     *float64 `json:"time"`
+		Text     string   `json:"text,omitempty"`
+	} `json:"data"`
 }
