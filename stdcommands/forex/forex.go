56445bd6de45ab1599cc5a2a4c090a69947470b7
Issacboi <40278713+issacboil@users.noreply.github.com>
2022-09-27 11:15:20 -0300

    Changed Command Names and a couple changes.
    Co-Authored-By: Shadow <66721908+Shadow21AR@users.noreply.github.com>

--- stdcommands/forex/forex.go
+++ stdcommands/forex/forex.go
@@ -1,4 +1,4 @@
-package exchange
+package forex
 
 import (
 	"encoding/json"
@@ -11,14 +11,17 @@ import (
 	"github.com/botlabs-gg/yagpdb/v2/commands"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
 	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 var Command = &commands.YAGCommand{
 	CmdCategory:         commands.CategoryFun,
 	Cooldown:            5,
-	Name:                "Exchange",
-	Aliases:             []string{"exch", "money"},
-	Description:         "💱Shows Currency Exchange Rates",
+	Name:                "Forex",
+	Aliases:             []string{"Money"},
+	Description:         "💱 convert value from one currency for another.",
 	RunInDM:             true,
 	DefaultEnabled:      true,
 	SlashCommandEnabled: true,
@@ -28,7 +31,7 @@ var Command = &commands.YAGCommand{
 	},
 
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		amount := data.Args[0].Str()
+		amount := data.Args[0]
 		check, err := requestAPI("https://api.exchangerate.host/symbols")
 		if err != nil {
 			return nil, err
@@ -38,22 +41,26 @@ var Command = &commands.YAGCommand{
 		if (to == nil) || (from == nil) {
 			return "Invalid currency code.\nCheck out available codes on: <https://api.exchangerate.host/symbols>", nil
 		}
-		output, err := requestAPI("https://api.exchangerate.host/convert?from=" + from.Code + "&to=" + to.Code + "&amount=" + amount)
+		output, err := requestAPI("https://api.exchangerate.host/convert?from=" + from.Code + "&to=" + to.Code + "&amount=" + amount.Str())
 		if err != nil {
 			return nil, err
 		}
+		if output.Info.Rate == 0 {
+			return "Something went wrong :c", err //VEF is bugged but i dont see any way to fix it yet. other than API fixing it themself :/
+		}
+		p := message.NewPrinter(language.English)
 		embed := &discordgo.MessageEmbed{
 			Title:       "💱Currency Exchange Rate",
-			Description: fmt.Sprintf("\n%s **%s** (%s) is %0.2f **%s** (%s).", amount, from.Description, output.Query.From, output.Result, to.Description, output.Query.To),
+			Description: fmt.Sprintf("\n%s **%s** (%s) is %s **%s** (%s).", p.Sprintf("%d", amount.Int64()), from.Description, output.Query.From, p.Sprintf("%0.2f", output.Result), to.Description, output.Query.To),
 			Color:       0xAE27FF,
-			Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Based on currency rate 1 : %0.3f", output.Result)},
-            Timestamp:   time.Now().UTC().Format(time.RFC3339),
+			Footer:      &discordgo.MessageEmbedFooter{Text: fmt.Sprintf("Based on currency rate 1 : %f", output.Info.Rate)},
+			Timestamp:   time.Now().UTC().Format(time.RFC3339),
 		}
 		return embed, nil
 	},
 }
 
-func requestAPI(query string) (*Result, error) {
+func requestAPI(query string) (*ExchangeAPIResult, error) {
 	req, err := http.NewRequest("GET", query, nil)
 	if err != nil {
 		return nil, err
@@ -71,7 +78,7 @@ func requestAPI(query string) (*Result, error) {
 	if err != nil {
 		return nil, err
 	}
-	result := &Result{}
+	result := &ExchangeAPIResult{}
 	err = json.Unmarshal([]byte(body), &result)
 	if err != nil {
 		return nil, err
@@ -79,12 +86,12 @@ func requestAPI(query string) (*Result, error) {
 	return result, nil
 }
 
-type SymbolInfo struct {
+type CurrencySymbolInfo struct {
 	Description string `json:"description,omitempty"`
 	Code        string `json:"code,omitempty"`
 }
 
-type Result struct {
+type ExchangeAPIResult struct {
 	Motd *struct {
 		Msg string `json:"msg"`
 		URL string `json:"url"`
@@ -98,8 +105,8 @@ type Result struct {
 	Info *struct {
 		Rate float64 `json:"rate"`
 	} `json:"info,omitempty"`
-	Historical bool                   `json:"historical,omitempty"`
-	Date       string                 `json:"date,omitempty"`
-	Result     float64                `json:"result,omitempty"`
-	Symbols    map[string]*SymbolInfo `json:"symbols,omitempty"`
+	Historical bool                           `json:"historical,omitempty"`
+	Date       string                         `json:"date,omitempty"`
+	Result     float64                        `json:"result,omitempty"`
+	Symbols    map[string]*CurrencySymbolInfo `json:"symbols,omitempty"`
 }
