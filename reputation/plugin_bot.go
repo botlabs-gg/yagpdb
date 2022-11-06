cb4a3a6bfd8ba5ac9779b5b13aac562f705cc1bc
Wolveric Catkin <72138328+Wolveric@users.noreply.github.com>
2022-11-06 22:09:17 +0000

    style(rep): Remove redundant semicolon

--- reputation/plugin_bot.go
+++ reputation/plugin_bot.go
@@ -36,7 +36,7 @@ func (p *Plugin) BotInit() {
 var thanksRegex = regexp.MustCompile(`(?i)( |\n|^)(thanks?\pP*|danks|ty|thx|\+rep|\+ ?\<\@[0-9]*\>)( |\n|$)`)
 
 func repDisabledError(guild *GuildContextData) string {
-	return fmt.Sprintf("**The reputation system is disabled for this server.** Enable it at: <https://yagpdb.xyz/manage/%d/reputation>.", guild.GS.ID);
+	return fmt.Sprintf("**The reputation system is disabled for this server.** Enable it at: <https://yagpdb.xyz/manage/%d/reputation>.", guild.GS.ID)
 }
 
 func handleMessageCreate(evt *eventsystem.EventData) {
