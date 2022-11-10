8560d7ca9bc9481fcd7317b21fed4be95c8d0905
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-11-10 16:50:38 +0530

    hidden username logging toggles in ui if it is disabled globally

--- logs/web.go
+++ logs/web.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -100,7 +101,7 @@ func (lp *Plugin) InitWeb() {
 func HandleLogsCP(w http.ResponseWriter, r *http.Request) (web.TemplateData, error) {
 	ctx := r.Context()
 	g, tmpl := web.GetBaseCPContextData(ctx)
-
+	tmpl["UserNickLoggingGlobalDisabled"] = os.Getenv("YAGPDB_LOGS_DISABLE_USERNAME_TRACKING") != ""
 	beforeID := 0
 	beforeStr := r.URL.Query().Get("before")
 	if beforeStr != "" {
