306a8c7179c0c727b28b8e70928608579968e219
Ranger <a.rhykerw@gmail.com>
2023-01-28 21:04:37 +0000

    forex: clear invalid currency code (#1383) (#1425)
    * Fancy Error Message
    
    * removed 'count' as it was unnecessary
    
    * fix(forex): Clearer error message
    
    * fix(forex): Trims leading whitespace before currency codes
    
    * fix(forex): Trims missing  whitespace after currency codes
    
    * style: Assign the currency code link  to variable
    
    * style: Assign calculate max pages to 'maxPages' variable
    
    ---------
    
    Co-authored-by: Shadow21AR <sado@DESKTOP-F39HB2A.localdomain>
    Co-authored-by: Shadow <66721908+Shadow21AR@users.noreply.github.com>

--- stdcommands/forex/forex.go
+++ stdcommands/forex/forex.go
@@ -4,10 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+
 	"net/http"
+	"sort"
 	"strings"
 	"time"
+	"math"
 
+	"github.com/botlabs-gg/yagpdb/v2/bot/paginatedmessages"
 	"github.com/botlabs-gg/yagpdb/v2/commands"
 	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
 	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
@@ -38,8 +42,17 @@ var Command = &commands.YAGCommand{
 		}
 		from := check.Symbols[strings.ToUpper(data.Args[1].Str())]
 		to := check.Symbols[strings.ToUpper(data.Args[2].Str())]
+		// Checks the max amount of pages by the number of symbols on each page (15)
+		maxPages := int(math.Ceil(float64(len(check.Symbols))/float64(15)))
 		if (to == nil) || (from == nil) {
-			return "Invalid currency code.\nCheck out available codes on: <https://api.exchangerate.host/symbols>", nil
+			_, err = paginatedmessages.CreatePaginatedMessage(
+				data.GuildData.GS.ID, data.ChannelID, 1, maxPages, func(p *paginatedmessages.PaginatedMessage, page int) (*discordgo.MessageEmbed, error) {
+					return errEmbed(check, page)
+				})
+			if err != nil {
+				return nil, err
+			}
+			return nil, nil
 		}
 		output, err := requestAPI(fmt.Sprintf("https://api.exchangerate.host/convert?from=%s&to=%s&amount=1", from.Code, to.Code))
 		if err != nil {
@@ -84,6 +97,31 @@ func requestAPI(query string) (*ExchangeAPIResult, error) {
 		return nil, err
 	}
 	return result, nil
+}
+
+func errEmbed(check *ExchangeAPIResult, page int) (*discordgo.MessageEmbed, error) {
+	desc := "CODE | Description\n------------------"
+	var exchangeSymbols string = "https://api.exchangerate.host/symbols"
+	codes := make([]string, 0, len(check.Symbols))
+	for k := range check.Symbols {
+		codes = append(codes, k)
+	}
+	sort.Strings(codes)
+	start := (page * 15) - 15
+	end := page * 15
+	for i, c := range codes {
+		if i < end && i >= start {
+			desc = fmt.Sprintf("%s\n%s  | %s", desc, c, check.Symbols[c].Description)
+		}
+	}
+	embed := &discordgo.MessageEmbed{
+		Title:       "Invalid currency code",
+		URL:         exchangeSymbols,
+		Color:       0xAE27FF,
+		Timestamp:   time.Now().UTC().Format(time.RFC3339),
+		Description: fmt.Sprintf("Check out available codes on: %s ```\n%s```", exchangeSymbols, desc),
+	}
+	return embed, nil
 }
 
 type CurrencySymbolInfo struct {
