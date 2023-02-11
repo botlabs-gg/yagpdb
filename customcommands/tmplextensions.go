2eeccda5fe7c1acdf4f87edbd95529e50542b2ab
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2023-02-12 03:30:12 +0530

    add execCC checks for schedule unique CC too

--- customcommands/tmplextensions.go
+++ customcommands/tmplextensions.go
@@ -276,6 +276,14 @@ func tmplScheduleUniqueCC(ctx *templates.Context) interface{} {
 			return "", errors.New("Couldn't find custom command")
 		}
 
+		if cmd.Disabled {
+			return "", errors.New("custom command is disabled")
+		}
+
+		if cmd.TriggerType == int(CommandTriggerInterval) {
+			return "", errors.New("interval custom command cannot be used with scheduleUniqueCC")
+		}
+
 		channelID := ctx.ChannelArg(channel)
 		if channelID == 0 {
 			return "", errors.New("Unknown channel")
