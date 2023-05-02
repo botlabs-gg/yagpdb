464485d3588c94f36071968cf34644472dcb5869
user9684 <user9684official@gmail.com>
2023-05-02 06:58:19 -0500

    Possible fix to panic

--- lib/confusables/confusables.go
+++ lib/confusables/confusables.go
@@ -14,23 +14,28 @@ import (
 
 // Replaces characters in the Unicode Table "Marks Nonspaced" and removes / replaces them.
 var transformer = transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
-var replacer strings.Replacer
+var confusablesReplacer strings.Replacer
+var diacriticReplacer strings.Replacer
 
 // Create strings replacer accordingly to unicode and "extraConfusables.json".
 func Init() {
-	fmt.Printf("Loading confusables..")
-	replacer = *strings.NewReplacer(confusables...)
+	fmt.Println("Loading confusables..")
+	confusablesReplacer = *strings.NewReplacer(confusables...)
 	fmt.Printf("Loaded %d confusables\n", len(confusables)/2)
+
+	fmt.Println("Loading diacritics..")
+	diacriticReplacer = *strings.NewReplacer(diacritics...)
+	fmt.Printf("Loaded %d confusables\n", len(diacritics)/2)
 }
 
 // SanitizeText normalizes text and matches confusables.
 // i.e. "Ĥéĺĺó" -> "Hello".
 func SanitizeText(content string) string {
-	// Normalize string
-	output, _, _ := transform.String(transformer, content)
+	// Normalize string.
+	output := diacriticReplacer.Replace(content)
 
-	// Match confusables
-	output = replacer.Replace(output)
+	// Match confusables.
+	output = confusablesReplacer.Replace(output)
 
 	return output
 }
