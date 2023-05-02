8c9e941cff27a52b177da3d4d96364b876e314c1
User <user@statusplus.xyz>
2023-05-02 10:32:06 -0500

    Merge replacers

--- lib/confusables/confusables.go
+++ lib/confusables/confusables.go
@@ -5,32 +5,20 @@ package confusables
 import (
 	"fmt"
 	"strings"
-	"unicode"
-
-	"golang.org/x/text/runes"
-	"golang.org/x/text/transform"
-	"golang.org/x/text/unicode/norm"
 )
 
-// Replaces characters in the Unicode Table "Marks Nonspaced" and removes / replaces them.
-var transformer = transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
 var replacer strings.Replacer
 
 // Create strings replacer accordingly to unicode and "extraConfusables.json".
 func Init() {
-	fmt.Printf("Loading confusables..")
-	replacer = *strings.NewReplacer(confusables...)
-	fmt.Printf("Loaded %d confusables\n", len(confusables)/2)
+	fmt.Println("Loading sanitizer...")
+	replacer = *strings.NewReplacer(append(confusables, diacritics...)...)
+	fmt.Printf("Loaded %d confusables and %d diacritics\n", len(confusables)/2, len(diacritics)/2)
 }
 
 // SanitizeText normalizes text and matches confusables.
 // i.e. "Ĥéĺĺó" -> "Hello".
 func SanitizeText(content string) string {
-	// Normalize string
-	output, _, _ := transform.String(transformer, content)
-
-	// Match confusables
-	output = replacer.Replace(output)
-
-	return output
+	// Normalize string.
+	return replacer.Replace(content)
 }
