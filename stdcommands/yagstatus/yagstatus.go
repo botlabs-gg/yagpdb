20b39528407bfe5e63a2b8bc9101ae4dacbe37e2
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2023-05-06 02:30:20 +0530

    more perm changes

--- stdcommands/yagstatus/yagstatus.go
+++ stdcommands/yagstatus/yagstatus.go
@@ -65,14 +65,14 @@ func cmdFuncYagStatus(data *dcmd.Data) (interface{}, error) {
 		},
 		Title: "YAGPDB Status, version " + common.VERSION,
 		Fields: []*discordgo.MessageEmbedField{
-			&discordgo.MessageEmbedField{Name: "Servers", Value: fmt.Sprint(servers), Inline: true},
-			&discordgo.MessageEmbedField{Name: "Go Version", Value: runtime.Version(), Inline: true},
-			&discordgo.MessageEmbedField{Name: "Uptime", Value: common.HumanizeDuration(common.DurationPrecisionSeconds, uptime), Inline: true},
-			&discordgo.MessageEmbedField{Name: "Goroutines", Value: fmt.Sprint(numGoroutines), Inline: true},
-			&discordgo.MessageEmbedField{Name: "GC Pause Fraction", Value: fmt.Sprintf("%.3f%%", memStats.GCCPUFraction*100), Inline: true},
-			&discordgo.MessageEmbedField{Name: "Process Mem (alloc, sys, freed)", Value: fmt.Sprintf("%.1fMB, %.1fMB, %.1fMB", float64(memStats.Alloc)/1000000, float64(memStats.Sys)/1000000, (float64(memStats.TotalAlloc)/1000000)-allocated), Inline: true},
-			&discordgo.MessageEmbedField{Name: "System Mem (used, total)", Value: sysMemStats, Inline: true},
-			&discordgo.MessageEmbedField{Name: "System Load (1, 5, 15)", Value: sysLoadStats, Inline: true},
+			{Name: "Servers", Value: fmt.Sprint(servers), Inline: true},
+			{Name: "Go Version", Value: runtime.Version(), Inline: true},
+			{Name: "Uptime", Value: common.HumanizeDuration(common.DurationPrecisionSeconds, uptime), Inline: true},
+			{Name: "Goroutines", Value: fmt.Sprint(numGoroutines), Inline: true},
+			{Name: "GC Pause Fraction", Value: fmt.Sprintf("%.3f%%", memStats.GCCPUFraction*100), Inline: true},
+			{Name: "Process Mem (alloc, sys, freed)", Value: fmt.Sprintf("%.1fMB, %.1fMB, %.1fMB", float64(memStats.Alloc)/1000000, float64(memStats.Sys)/1000000, (float64(memStats.TotalAlloc)/1000000)-allocated), Inline: true},
+			{Name: "System Mem (used, total)", Value: sysMemStats, Inline: true},
+			{Name: "System Load (1, 5, 15)", Value: sysLoadStats, Inline: true},
 		},
 	}
 
