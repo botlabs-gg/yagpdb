bcccae4d688f4024cb75b4ad6c2953c5b040a6dd
Wolveric <wolveric.catkin@gmail.com>
2023-05-08 04:14:50 +0100

    feat(youtube): Add live sharing URL parsing
    This commit adds support for adding YouTube channels,
    using a YouTube URL, with the `live` path segment.

--- youtube/feed.go
+++ youtube/feed.go
@@ -367,7 +367,7 @@ func (p *Plugin) parseYtUrl(channelUrl *url.URL) (id ytChannelID, err error) {
 	second := pathSegments[1]
 
 	switch first {
-	case "shorts":
+	case "shorts", "live":
 		return p.parseYtVideoID(second)
 	case "channel":
 		if ytChannelIDRegex.MatchString(second) {
