4c51c933712c6db990a5b5c7165c6ba371c4ed5a
savage4618 <savage4618@users.noreply.github.com>
2024-02-29 19:37:42 -0500

    updated help description to show bot admin only

--- stdcommands/topservers/topservers.go
+++ stdcommands/topservers/topservers.go
@@ -15,7 +15,7 @@ var Command = &commands.YAGCommand{
 	Cooldown:    5,
 	CmdCategory: commands.CategoryDebug,
 	Name:        "TopServers",
-	Description: "Responds with the top 20 servers I'm on",
+	Description: "Responds with the top 20 servers I'm on. *Bot admin only.",
 	Arguments: []*dcmd.ArgDef{
 		{Name: "Skip", Help: "Entries to skip", Type: dcmd.Int, Default: 0},
 	},
