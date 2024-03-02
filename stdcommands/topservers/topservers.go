e218da7adb01097acb6c2513e634ec0822927aa5
savage4618 <savage4618@users.noreply.github.com>
2024-03-02 01:03:21 -0500

    Clarification of topservers command (#1618)
    * moved category for top servers to debug&Maint
    
    * updated help description to show bot admin only

--- stdcommands/topservers/topservers.go
+++ stdcommands/topservers/topservers.go
@@ -13,9 +13,9 @@ import (
 
 var Command = &commands.YAGCommand{
 	Cooldown:    5,
-	CmdCategory: commands.CategoryFun,
+	CmdCategory: commands.CategoryDebug,
 	Name:        "TopServers",
-	Description: "Responds with the top 20 servers I'm on",
+	Description: "Responds with the top 20 servers I'm on. *Bot admin only.",
 	Arguments: []*dcmd.ArgDef{
 		{Name: "Skip", Help: "Entries to skip", Type: dcmd.Int, Default: 0},
 	},
