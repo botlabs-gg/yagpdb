7873bda07a18c539acb434f92a0a428f343c2748
SoggySaussages <vmdmaharaj@gmail.com>
2024-04-01 02:21:29 -0700

    Update cleardm.go
    This reverts commit 376513d6b4effbd5d2dfdced5c522dc419628293.

--- stdcommands/cleardm/cleardm.go
+++ stdcommands/cleardm/cleardm.go
@@ -3,11 +3,11 @@ package cleardm
 import (
 	"fmt"
 
-	"github.com/botlabs-gg/yagpdb/v2/commands"
-	"github.com/botlabs-gg/yagpdb/v2/common"
-	"github.com/botlabs-gg/yagpdb/v2/lib/dcmd"
-	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
-	"github.com/botlabs-gg/yagpdb/v2/stdcommands/util"
+	"github.com/botlabs-gg/quackpdb/v2/commands"
+	"github.com/botlabs-gg/quackpdb/v2/common"
+	"github.com/botlabs-gg/quackpdb/v2/lib/dcmd"
+	"github.com/botlabs-gg/quackpdb/v2/lib/discordgo"
+	"github.com/botlabs-gg/quackpdb/v2/stdcommands/util"
 	"github.com/sirupsen/logrus"
 )
 
