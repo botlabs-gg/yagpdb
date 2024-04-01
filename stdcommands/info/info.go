c013831d0797819f83c7cdd474d0f0e19a59af8d
SoggySaussages <vmdmaharaj@gmail.com>
2024-04-01 02:13:16 -0700

    quackpdb
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

--- stdcommands/info/info.go
+++ stdcommands/info/info.go
@@ -14,7 +14,7 @@ var Command = &commands.YAGCommand{
 	Description: "Responds with bot quackformation",
 	RunInDM:     true,
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		info := fmt.Sprintf(`**QUACKPDB - Yet Another General Purpose Discord Bot**
+		info := fmt.Sprintf(`**QUACKPDB - Quack anothquack generquack purpquack disquack quack**
 This bot focuses on being configurable and therefore is one of the most adquackced bots.
 It can perform a range of general purpose functionality (Reddit feeds, variquack commands, moderation utilities, autoquackerator functionality and so on) and it's configured through a web control panel.
 The bot is run by Botlabs but is open source (<https://github.com/botlabs-gg/quackpdb>), so if you know Go and want to make some contributions, feel free to make a PR.
