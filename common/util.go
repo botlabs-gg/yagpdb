6471f828ae9be8f0a4e46a9a73ed8c42c2a0d2a6
Joe <jo3.l.dev@outlook.com>
2024-05-24 18:37:05 -0700

    all: migrate executed command logging to sqlboiler

--- common/util.go
+++ common/util.go
@@ -354,28 +354,6 @@ func IsDiscordErr(err error, codes ...int) bool {
 	return false
 }
 
-type LoggedExecutedCommand struct {
-	SmallModel
-
-	UserID    string
-	ChannelID string
-	GuildID   string
-
-	// Name of command that was triggered
-	Command string
-	// Raw command with arguments passed
-	RawCommand string
-	// If command returned any error this will be no-empty
-	Error string
-
-	TimeStamp    time.Time
-	ResponseTime int64
-}
-
-func (l LoggedExecutedCommand) TableName() string {
-	return "executed_commands"
-}
-
 func HumanizePermissions(perms int64) (res []string) {
 	if perms&discordgo.PermissionAdministrator == discordgo.PermissionAdministrator {
 		res = append(res, "Administrator")
