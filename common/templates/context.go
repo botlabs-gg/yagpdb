136b9ef3ac6a5b6f8196933d33f88eea636ed8d5
Pedro Henrique Pessoa <phenpessoa@gmail.com>
2024-05-31 15:18:20 -0300

    templates: update role funcs (#883)
    * templates: rework role funcs
    
    * small fixes
    
    * handle discordgo.Role and *discordgo.Role
    
    * fix conflicts
    
    * remove breaking changes and formatting
    
    ---------
    
    Co-authored-by: Ashish <51633862+ashishjh-bst@users.noreply.github.com>
    Co-authored-by: Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>

--- common/templates/context.go
+++ common/templates/context.go
@@ -591,28 +591,41 @@ func baseContextFuncs(c *Context) {
 	// Mentions
 	c.addContextFunc("mentionEveryone", c.tmplMentionEveryone)
 	c.addContextFunc("mentionHere", c.tmplMentionHere)
+	c.addContextFunc("mentionRole", c.tmplMentionRole)
+	c.addContextFunc("mentionRoleName", c.tmplMentionRoleName)
 	c.addContextFunc("mentionRoleID", c.tmplMentionRoleID)
 	c.addContextFunc("mentionRoleName", c.tmplMentionRoleName)
 
 	// Role functions
+	c.addContextFunc("getRole", c.tmplGetRole)
+	c.addContextFunc("getRoleID", c.tmplGetRoleID)
+	c.addContextFunc("getRoleName", c.tmplGetRoleName)
+
+	c.addContextFunc("hasRole", c.tmplHasRole)
 	c.addContextFunc("hasRoleID", c.tmplHasRoleID)
 	c.addContextFunc("hasRoleName", c.tmplHasRoleName)
 
-	c.addContextFunc("addRoleID", c.tmplAddRoleID)
-	c.addContextFunc("removeRoleID", c.tmplRemoveRoleID)
+	c.addContextFunc("targetHasRole", c.tmplTargetHasRole)
+	c.addContextFunc("targetHasRoleID", c.tmplTargetHasRoleID)
+	c.addContextFunc("targetHasRoleName", c.tmplTargetHasRoleName)
 
-	c.addContextFunc("setRoles", c.tmplSetRoles)
-	c.addContextFunc("addRoleName", c.tmplAddRoleName)
-	c.addContextFunc("removeRoleName", c.tmplRemoveRoleName)
-
+	c.addContextFunc("giveRole", c.tmplGiveRole)
 	c.addContextFunc("giveRoleID", c.tmplGiveRoleID)
 	c.addContextFunc("giveRoleName", c.tmplGiveRoleName)
 
+	c.addContextFunc("addRole", c.tmplAddRole)
+	c.addContextFunc("addRoleID", c.tmplAddRoleID)
+	c.addContextFunc("addRoleName", c.tmplAddRoleName)
+
+	c.addContextFunc("takeRole", c.tmplTakeRole)
 	c.addContextFunc("takeRoleID", c.tmplTakeRoleID)
 	c.addContextFunc("takeRoleName", c.tmplTakeRoleName)
 
-	c.addContextFunc("targetHasRoleID", c.tmplTargetHasRoleID)
-	c.addContextFunc("targetHasRoleName", c.tmplTargetHasRoleName)
+	c.addContextFunc("removeRole", c.tmplRemoveRole)
+	c.addContextFunc("removeRoleID", c.tmplRemoveRoleID)
+	c.addContextFunc("removeRoleName", c.tmplRemoveRoleName)
+
+	c.addContextFunc("setRoles", c.tmplSetRoles)
 
 	// permission funcs
 	c.addContextFunc("hasPermissions", c.tmplHasPermissions)
@@ -630,6 +643,10 @@ func baseContextFuncs(c *Context) {
 	c.addContextFunc("getChannel", c.tmplGetChannel)
 	c.addContextFunc("getChannelPins", c.tmplGetChannelPins(false))
 	c.addContextFunc("getChannelOrThread", c.tmplGetChannelOrThread)
+	c.addContextFunc("getPinCount", c.tmplGetChannelPins(true))
+	c.addContextFunc("addReactions", c.tmplAddReactions)
+	c.addContextFunc("addResponseReactions", c.tmplAddResponseReactions)
+	c.addContextFunc("addMessageReactions", c.tmplAddMessageReactions)
 	c.addContextFunc("getMember", c.tmplGetMember)
 	c.addContextFunc("getMessage", c.tmplGetMessage)
 	c.addContextFunc("getPinCount", c.tmplGetChannelPins(true))
