6e98c9886a8ea0225297e8dc1fae3b72b8860ed5
Joe L <56809242+jo3-l@users.noreply.github.com>
2024-06-15 22:04:29 -0700

    notifications: migrate away from gorm to sqlboiler (#1659)
    * notifications: decouple from configstore
    
    * notifications: add database schemas
    
    * notifications: set up and run sqlboiler
    
    * notifications: migrate to sqlboiler

---
+++ notifications/config.go
@@ -0,0 +1,120 @@
+package notifications
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
+	"github.com/botlabs-gg/yagpdb/v2/notifications/models"
+	"github.com/botlabs-gg/yagpdb/v2/web"
+	"github.com/volatiletech/null/v8"
+)
+
+// For legacy reasons, many columns in the database schema are marked as
+// nullable when they should really be non-nullable, meaning working with
+// models.GeneralNotificationConfig directly is much more annoying than it
+// should be. We therefore wrap it in a Config (which has proper types) and
+// convert to/from only when strictly required.
+
+type Config struct {
+	GuildID   int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	JoinServerEnabled bool  `json:"join_server_enabled" schema:"join_server_enabled"`
+	JoinServerChannel int64 `json:"join_server_channel" schema:"join_server_channel" valid:"channel,true"`
+
+	JoinServerMsgs []string `json:"join_server_msgs" schema:"join_server_msgs" valid:"template,5000"`
+	JoinDMEnabled  bool     `json:"join_dm_enabled" schema:"join_dm_enabled"`
+	JoinDMMsg      string   `json:"join_dm_msg" schema:"join_dm_msg" valid:"template,5000"`
+
+	LeaveEnabled bool     `json:"leave_enabled" schema:"leave_enabled"`
+	LeaveChannel int64    `json:"leave_channel" schema:"leave_channel" valid:"channel,true"`
+	LeaveMsgs    []string `json:"leave_msgs" schema:"leave_msgs" valid:"template,5000"`
+
+	TopicEnabled bool  `json:"topic_enabled" schema:"topic_enabled"`
+	TopicChannel int64 `json:"topic_channel" schema:"topic_channel" valid:"channel,true"`
+
+	CensorInvites bool `schema:"censor_invites"`
+}
+
+var _ web.CustomValidator = (*Config)(nil)
+
+const MaxResponses = 10
+
+func (c *Config) Validate(tmpl web.TemplateData, _ int64) bool {
+	if len(c.JoinServerMsgs) > MaxResponses {
+		tmpl.AddAlerts(web.ErrorAlert(fmt.Sprintf("Too many join server messages, max %d", MaxResponses)))
+		return false
+	}
+	if len(c.LeaveMsgs) > MaxResponses {
+		tmpl.AddAlerts(web.ErrorAlert(fmt.Sprintf("Too many leave server messages, max %d", MaxResponses)))
+		return false
+	}
+	return true
+}
+
+// For legacy reasons, the JoinServerMsgs and LeaveMsgs columns are not stored
+// as real TEXT[] columns in database but rather single TEXT columns separated
+// by U+001E (INFORMATION SEPARATOR TWO.)
+
+const RecordSeparator = "\x1e"
+
+func readLegacyMultiResponseColumn(s string) []string {
+	return strings.Split(s, RecordSeparator)
+}
+func writeLegacyMultiResponseColumn(responses []string) string {
+	return strings.Join(responses, RecordSeparator)
+}
+
+func (c *Config) ToModel() *models.GeneralNotificationConfig {
+	return &models.GeneralNotificationConfig{
+		GuildID:   c.GuildID,
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
+
+		JoinServerEnabled: null.BoolFrom(c.JoinServerEnabled),
+		JoinServerChannel: null.StringFrom(discordgo.StrID(c.JoinServerChannel)),
+
+		JoinServerMsgs: null.StringFrom(writeLegacyMultiResponseColumn(c.JoinServerMsgs)),
+		JoinDMEnabled:  null.BoolFrom(c.JoinDMEnabled),
+		JoinDMMsg:      null.StringFrom(c.JoinDMMsg),
+
+		LeaveEnabled: null.BoolFrom(c.LeaveEnabled),
+		LeaveChannel: null.StringFrom(discordgo.StrID(c.LeaveChannel)),
+		LeaveMsgs:    null.StringFrom(writeLegacyMultiResponseColumn(c.LeaveMsgs)),
+
+		TopicEnabled: null.BoolFrom(c.TopicEnabled),
+		TopicChannel: null.StringFrom(discordgo.StrID(c.TopicChannel)),
+
+		CensorInvites: null.BoolFrom(c.CensorInvites),
+	}
+}
+
+func configFromModel(model *models.GeneralNotificationConfig) *Config {
+	joinServerChannel, _ := discordgo.ParseID(model.JoinServerChannel.String)
+	leaveChannel, _ := discordgo.ParseID(model.LeaveChannel.String)
+	topicChannel, _ := discordgo.ParseID(model.TopicChannel.String)
+	return &Config{
+		GuildID:   model.GuildID,
+		CreatedAt: model.CreatedAt,
+		UpdatedAt: model.UpdatedAt,
+
+		JoinServerEnabled: model.JoinServerEnabled.Bool,
+		JoinServerChannel: joinServerChannel,
+
+		JoinServerMsgs: readLegacyMultiResponseColumn(model.JoinServerMsgs.String),
+		JoinDMEnabled:  model.JoinDMEnabled.Bool,
+		JoinDMMsg:      model.JoinDMMsg.String,
+
+		LeaveEnabled: model.LeaveEnabled.Bool,
+		LeaveChannel: leaveChannel,
+		LeaveMsgs:    readLegacyMultiResponseColumn(model.LeaveMsgs.String),
+
+		TopicEnabled: model.TopicEnabled.Bool,
+		TopicChannel: topicChannel,
+
+		CensorInvites: model.CensorInvites.Bool,
+	}
+}
--- notifications/notifications.go
+++ notifications/notifications.go
@@ -1,19 +1,10 @@
 package notifications
 
 import (
-	"strconv"
-	"strings"
-
-	"emperror.dev/errors"
 	"github.com/botlabs-gg/yagpdb/v2/common"
-	"github.com/botlabs-gg/yagpdb/v2/common/configstore"
-	"golang.org/x/net/context"
 )
 
-const (
-	RecordSeparator = "\x1e"
-	MaxUserMessages = 10
-)
+//go:generate sqlboiler --no-hooks psql
 
 var logger = common.GetPluginLogger(&Plugin{})
 
@@ -23,9 +14,7 @@ func RegisterPlugin() {
 	plugin := &Plugin{}
 	common.RegisterPlugin(plugin)
 
-	common.GORM.AutoMigrate(&Config{})
-	configstore.RegisterConfig(configstore.SQL, &Config{})
-
+	common.InitSchemas("notifications", DBSchemas...)
 }
 
 func (p *Plugin) PluginInfo() *common.PluginInfo {
@@ -34,134 +23,4 @@ func (p *Plugin) PluginInfo() *common.PluginInfo {
 		SysName:  "notifications",
 		Category: common.PluginCategoryFeeds,
 	}
-}
-
-type Config struct {
-	configstore.GuildConfigModel
-	JoinServerEnabled bool   `json:"join_server_enabled" schema:"join_server_enabled"`
-	JoinServerChannel string `json:"join_server_channel" schema:"join_server_channel" valid:"channel,true"`
-
-	// Implementation note: gorilla/schema currently requires manual index
-	// setting in forms to parse sub-objects. GORM has_many is also complicated
-	// by manual handling of associations and loss of IDs through the web form
-	// (without which Replace() is currently n^2).
-	// For strings, we greatly simplify things by flattening for storage.
-
-	// TODO: Remove the legacy single-message variant when ready to migrate the
-	// database.
-	JoinServerMsg  string   `json:"join_server_msg" valid:"template,5000"`
-	JoinServerMsgs []string `json:"join_server_msgs" schema:"join_server_msgs" gorm:"-" valid:"template,5000"`
-	// Do Not Use! For persistence only.
-	JoinServerMsgs_ string `json:"-"`
-
-	JoinDMEnabled bool   `json:"join_dm_enabled" schema:"join_dm_enabled"`
-	JoinDMMsg     string `json:"join_dm_msg" schema:"join_dm_msg" valid:"template,5000"`
-
-	LeaveEnabled bool     `json:"leave_enabled" schema:"leave_enabled"`
-	LeaveChannel string   `json:"leave_channel" schema:"leave_channel" valid:"channel,true"`
-	LeaveMsg     string   `json:"leave_msg" schema:"leave_msg" valid:"template,5000"`
-	LeaveMsgs    []string `json:"leave_msgs" schema:"leave_msgs" gorm:"-" valid:"template,5000"`
-	// Do Not Use! For persistence only.
-	LeaveMsgs_ string `json:"-"`
-
-	TopicEnabled bool   `json:"topic_enabled" schema:"topic_enabled"`
-	TopicChannel string `json:"topic_channel" schema:"topic_channel" valid:"channel,true"`
-
-	CensorInvites bool `schema:"censor_invites"`
-}
-
-func (c *Config) JoinServerChannelInt() (i int64) {
-	i, _ = strconv.ParseInt(c.JoinServerChannel, 10, 64)
-	return
-}
-
-func (c *Config) LeaveChannelInt() (i int64) {
-	i, _ = strconv.ParseInt(c.LeaveChannel, 10, 64)
-	return
-}
-
-func (c *Config) TopicChannelInt() (i int64) {
-	i, _ = strconv.ParseInt(c.TopicChannel, 10, 64)
-	return
-}
-
-func (c *Config) GetName() string {
-	return "general_notifications"
-}
-
-func (c *Config) TableName() string {
-	return "general_notification_configs"
-}
-
-// GORM BeforeSave hook
-func (c *Config) BeforeSave() (err error) {
-	filterAndJoin := func(a []string) string {
-		joined := ""
-		msgsJoined := 0
-		for _, s := range a {
-			if s == "" {
-				continue
-			}
-			if msgsJoined >= MaxUserMessages {
-				break
-			}
-			msgsJoined++
-
-			if len(joined) > 0 {
-				joined += RecordSeparator
-			}
-
-			joined += s
-		}
-
-		return joined
-	}
-
-	c.JoinServerMsgs_ = filterAndJoin(c.JoinServerMsgs)
-	c.LeaveMsgs_ = filterAndJoin(c.LeaveMsgs)
-
-	return nil
-}
-
-// GORM AfterFind hook
-func (c *Config) AfterFind() (err error) {
-	if c.JoinServerMsg != "" {
-		c.JoinServerMsgs = append(c.JoinServerMsgs, c.JoinServerMsg)
-		c.JoinServerMsg = ""
-	}
-	if c.JoinServerMsgs_ != "" {
-		c.JoinServerMsgs = append(c.JoinServerMsgs, strings.Split(c.JoinServerMsgs_, RecordSeparator)...)
-	}
-
-	if c.LeaveMsg != "" {
-		c.LeaveMsgs = append(c.LeaveMsgs, c.LeaveMsg)
-		c.LeaveMsg = ""
-	}
-	if c.LeaveMsgs_ != "" {
-		c.LeaveMsgs = append(c.LeaveMsgs, strings.Split(c.LeaveMsgs_, RecordSeparator)...)
-	}
-
-	return nil
-}
-
-var DefaultConfig = &Config{}
-
-func GetConfig(guildID int64) (*Config, error) {
-	var conf Config
-	err := configstore.Cached.GetGuildConfig(context.Background(), guildID, &conf)
-	if err == nil {
-		return &conf, nil
-	}
-
-	if err == configstore.ErrNotFound {
-		// if err != configstore.ErrNotFound {
-		// 	log.WithError(err).Error("Failed retrieving config")
-		// }
-		return &Config{
-			JoinServerMsgs: []string{"<@{{.User.ID}}> Joined!"},
-			LeaveMsgs:      []string{"**{{.User.Username}}** Left... :'("},
-		}, nil
-	}
-
-	return nil, errors.WithStackIf(err)
 }
