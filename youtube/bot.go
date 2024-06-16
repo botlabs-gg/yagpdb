10614c26845db49e8749cec94a65e8a0845979f9
Joe L <56809242+jo3-l@users.noreply.github.com>
2024-06-15 22:03:33 -0700

    youtube: migrate away from gorm to sqlboiler (#1660)
    * youtube: add database schemas
    
    * youtube: set up and run sqlboiler
    
    * youtube: migrate to sqlboiler

--- youtube/bot.go
+++ youtube/bot.go
@@ -1,39 +1,39 @@
 package youtube
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/botlabs-gg/yagpdb/v2/common"
+	"github.com/botlabs-gg/yagpdb/v2/lib/discordgo"
+	"github.com/botlabs-gg/yagpdb/v2/youtube/models"
 	"github.com/mediocregopher/radix/v3"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 func (p *Plugin) Status() (string, string) {
 	var unique int
 	common.RedisPool.Do(radix.Cmd(&unique, "ZCARD", RedisKeyWebSubChannels))
 
-	var numChannels int
-	common.GORM.Model(&ChannelSubscription{}).Count(&numChannels)
+	total, _ := models.YoutubeChannelSubscriptions().CountG(context.Background())
 
-	return "Unique/Total", fmt.Sprintf("%d/%d", unique, numChannels)
+	return "Unique/Total", fmt.Sprintf("%d/%d", unique, total)
 }
 
 func (p *Plugin) OnRemovedPremiumGuild(guildID int64) error {
-	logger.WithField("guild_id", guildID).Infof("Removed Excess Youtube Feeds")
-	feeds := make([]ChannelSubscription, 0)
-	err := common.GORM.Model(&ChannelSubscription{}).Where(`guild_id = ? and enabled = ?`, guildID, common.BoolToPointer(true)).Offset(GuildMaxFeeds).Order(
-		"id desc",
-	).Find(&feeds).Error
+	numDisabled, err := models.YoutubeChannelSubscriptions(
+		models.YoutubeChannelSubscriptionWhere.GuildID.EQ(discordgo.StrID(guildID)),
+		models.YoutubeChannelSubscriptionWhere.Enabled.EQ(true),
+
+		qm.Offset(GuildMaxFeeds),
+		qm.OrderBy("id DESC"),
+	).UpdateAllG(context.Background(), models.M{"enabled": false})
+
 	if err != nil {
-		logger.WithError(err).Errorf("failed getting feed ids for guild_id %d", guildID)
+		logger.WithError(err).WithField("guild", guildID).Error("failed disabling excess feeds")
 		return err
 	}
 
-	if len(feeds) > 0 {
-		err = common.GORM.Model(&feeds).Update(ChannelSubscription{Enabled: common.BoolToPointer(false)}).Error
-		if err != nil {
-			logger.WithError(err).Errorf("failed getting feed ids for guild_id %d", guildID)
-			return err
-		}
-	}
+	logger.WithField("guild", guildID).Infof("disabled %d excess feeds", numDisabled)
 	return nil
 }
