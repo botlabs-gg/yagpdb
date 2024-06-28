707592a1ed7007314df11b303a27d1b07473cf09
Borbot33 <58076174+Borbot33@users.noreply.github.com>
2024-06-28 19:30:05 +0200

    trigger automod using slashcommands

--- automod/automod_bot.go
+++ automod/automod_bot.go
@@ -46,10 +46,10 @@ func (p *Plugin) handleMsgUpdate(evt *eventsystem.EventData) {
 
 // called on new messages and edits
 func (p *Plugin) checkMessage(evt *eventsystem.EventData, msg *discordgo.Message) bool {
-	if !bot.IsNormalUserMessage(msg) {
+	if msg.Author == nil || msg.Author.ID == common.BotUser.ID || msg.Author.Discriminator == "0000" || (msg.Member == nil && msg.GuildID != 0) {
 		return false
 	}
-
+	
 	if !evt.HasFeatureFlag(featureFlagEnabled) || msg.GuildID == 0 {
 		return true
 	}
@@ -59,7 +59,11 @@ func (p *Plugin) checkMessage(evt *eventsystem.EventData, msg *discordgo.Message
 		return true
 	}
 
-	ms := dstate.MemberStateFromMember(msg.Member)
+	ms := &dstate.MemberState{
+		GuildID: msg.GuildID,
+		Member:  bot.MemberToMemberFields(msg.Member),
+		User:    *msg.Author,
+	}
 
 	stripped := ""
 	return !p.CheckTriggers(nil, evt.GS, ms, msg, cs, func(trig *ParsedPart) (activated bool, err error) {
