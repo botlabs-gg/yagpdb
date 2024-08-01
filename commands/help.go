eab57e998c9084ba27a1b0cbbc4f7cb5a3af4a1b
Ashish <51633862+ashishjh-bst@users.noreply.github.com>
2024-08-01 23:03:45 +0530

    Update links to point to new docs and changed youtube custom notification text

--- commands/help.go
+++ commands/help.go
@@ -97,14 +97,14 @@ func cmdFuncHelp(data *dcmd.Data) (interface{}, error) {
 func createInteractiveHelp(userID int64, helpEmbeds []*discordgo.MessageEmbed) (interface{}, error) {
 	channel, err := common.BotSession.UserChannelCreate(userID)
 	if err != nil {
-		return "Something went wrong, maybe you have DMs disabled? I don't want to spam this channel so here's a external link to available commands: <https://docs.yagpdb.xyz/commands>", err
+		return "Something went wrong, maybe you have DMs disabled? I don't want to spam this channel so here's a external link to available commands: <https://help.yagpdb.xyz/docs/core/all-commands/>", err
 	}
 
 	// prepend a introductionairy first page
 	firstPage := &discordgo.MessageEmbed{
 		Title: "YAGPDB Help!",
 		Description: fmt.Sprintf(`YAGPDB is an open-source multipurpose discord bot that is configured through the web interface at %s.
-For more in depth help and information you should visit https://docs.yagpdb.xyz/ as this command only shows information about commands.)
+For more in depth help and information you should visit https://help.yagpdb.xyz/ as this command only shows information about commands.)
 		
 		
 **Use the emojis under to change pages**`, web.BaseURL()),
