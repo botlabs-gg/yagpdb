ce75680a72b22b0e6500fb6cf1fa15e05501b291
SoggySaussages <vmdmaharaj@gmail.com>
2024-11-24 23:48:56 -0800

    cc/cron: add cron testing
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

--- customcommands/interval_test.go
+++ customcommands/interval_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestNextRunTimeBasic(t *testing.T) {
 	cc := &models.CustomCommand{
+		TriggerType:         int(CommandTriggerInterval),
 		TimeTriggerInterval: 5,
 	}
 
@@ -29,10 +30,23 @@ func TestNextRunTimeBasic(t *testing.T) {
 	if next != expected {
 		t.Error("incorrect next run, should be: ", expected, ", got: ", next)
 	}
+
+	// cron
+
+	cc.TriggerType = int(CommandTriggerCron)
+	cc.TextTrigger = "5 * * * *"
+
+	next = CalcNextRunTime(cc, tim)
+	expected = tim.UTC().Add(time.Minute * 5)
+
+	if next != expected {
+		t.Error("incorrect next run, should be: ", expected, ", got: ", nextRun)
+	}
 }
 
 func TestNextRunTimeImpossible(t *testing.T) {
 	cc := &models.CustomCommand{
+		TriggerType:               int(CommandTriggerInterval),
 		TimeTriggerInterval:       1,
 		TimeTriggerExcludingHours: []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
 	}
@@ -61,10 +75,49 @@ func TestNextRunTimeImpossible(t *testing.T) {
 	if !next.IsZero() {
 		t.Error("next time is not zero: ", next)
 	}
+
+	// cron
+
+	cc.TriggerType = int(CommandTriggerCron)
+	cc.TextTrigger = "* * * * *"
+
+	next = CalcNextRunTime(cc, time.Now())
+	if !next.IsZero() {
+		t.Error("next time is not zero: ", next)
+	}
+
+	cc.TimeTriggerExcludingHours = nil
+	cc.TimeTriggerExcludingDays = []int64{0, 1, 2, 3, 4, 5, 6}
+
+	next = CalcNextRunTime(cc, time.Now())
+	if !next.IsZero() {
+		t.Error("next time is not zero: ", next)
+	}
+
+	cc.TimeTriggerExcludingDays = nil
+	next = CalcNextRunTime(cc, time.Now())
+	if !next.IsZero() {
+		t.Error("next time is not zero: ", next)
+	}
+
+	cc.TextTrigger = "* 0 * * *"
+	cc.TimeTriggerExcludingHours = []int64{0}
+	next = CalcNextRunTime(cc, time.Now())
+	if !next.IsZero() {
+		t.Error("next time is not zero: ", next)
+	}
+
+	cc.TextTrigger = "* * * * 0"
+	cc.TimeTriggerExcludingDays = []int64{0}
+	next = CalcNextRunTime(cc, time.Now())
+	if !next.IsZero() {
+		t.Error("next time is not zero: ", next)
+	}
 }
 
 func TestNextRunTimeExcludingHours(t *testing.T) {
 	cc := &models.CustomCommand{
+		TriggerType:               int(CommandTriggerInterval),
 		TimeTriggerInterval:       5,
 		TimeTriggerExcludingHours: []int64{0, 1},
 	}
@@ -77,10 +130,22 @@ func TestNextRunTimeExcludingHours(t *testing.T) {
 	if nextRun != expected {
 		t.Error("next run should be now: ", expected, ", got: ", nextRun)
 	}
+
+	// cron
+
+	cc.TriggerType = int(CommandTriggerCron)
+	cc.TextTrigger = "5 * * * *"
+
+	nextRun = CalcNextRunTime(cc, tim)
+
+	if nextRun != expected {
+		t.Error("next run should be now: ", expected, ", got: ", nextRun)
+	}
 }
 
 func TestNextRunTimeExcludingDays(t *testing.T) {
 	cc := &models.CustomCommand{
+		TriggerType:              int(CommandTriggerInterval),
 		TimeTriggerInterval:      5,
 		TimeTriggerExcludingDays: []int64{0, 1},
 	}
@@ -93,10 +158,22 @@ func TestNextRunTimeExcludingDays(t *testing.T) {
 	if nextRun != expected {
 		t.Error("next run should be now: ", expected, ", got: ", nextRun, tim.Weekday())
 	}
+
+	// cron
+
+	cc.TriggerType = int(CommandTriggerCron)
+	cc.TextTrigger = "5 * * * *"
+
+	nextRun = CalcNextRunTime(cc, tim)
+
+	if nextRun != expected {
+		t.Error("next run should be now: ", expected, ", got: ", nextRun)
+	}
 }
 
 func TestNextRunTimeExcludingDaysHours(t *testing.T) {
 	cc := &models.CustomCommand{
+		TriggerType:               int(CommandTriggerInterval),
 		TimeTriggerInterval:       5,
 		TimeTriggerExcludingDays:  []int64{2},
 		TimeTriggerExcludingHours: []int64{0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23},
@@ -108,6 +185,17 @@ func TestNextRunTimeExcludingDaysHours(t *testing.T) {
 
 	nextRun := CalcNextRunTime(cc, tim)
 	expected := tim.Add(time.Hour * 47)
+
+	if nextRun != expected {
+		t.Errorf("next run should be: %s (w:%d) got %s (w:%d - %d)", expected, expected.Weekday(), nextRun, int(nextRun.Weekday()), nextRun.Hour())
+	}
+
+	// cron
+
+	cc.TriggerType = int(CommandTriggerCron)
+	cc.TextTrigger = "5 * * * *"
+
+	nextRun = CalcNextRunTime(cc, tim)
 
 	if nextRun != expected {
 		t.Errorf("next run should be: %s (w:%d) got %s (w:%d - %d)", expected, expected.Weekday(), nextRun, int(nextRun.Weekday()), nextRun.Hour())
