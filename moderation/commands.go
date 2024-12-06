3c188bd93729452b1e3253ba923404f46103480b
Wolveric <wolveric.catkin@gmail.com>
2024-12-06 20:35:33 +0000

    refact(moderation): Allow non-ephemeral kick msg

--- moderation/commands.go
+++ moderation/commands.go
@@ -281,7 +281,7 @@ var ModerationCommands = []*commands.YAGCommand{
 		},
 		RequireBotPerms:     [][]int64{{discordgo.PermissionAdministrator}, {discordgo.PermissionKickMembers}},
 		SlashCommandEnabled: true,
-		IsResponseEphemeral: true,
+		IsResponseEphemeral: false,
 		RunFunc: func(parsed *dcmd.Data) (interface{}, error) {
 			if parsed.Context().Value(commands.CtxKeyExecutedByNestedCommandTemplate) == true {
 				return nil, errors.New("cannot nest exec/execAdmin calls")
