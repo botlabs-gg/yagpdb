2973f0970edf140c0a99ad19af17ca8a28ffafc0
Aresiel <25839246+Aresiel@users.noreply.github.com>
2024-12-12 18:57:21 +0100

    Ordered baseContextFuncs alphabetically

--- common/templates/context.go
+++ common/templates/context.go
@@ -656,8 +656,8 @@ func baseContextFuncs(c *Context) {
 	c.addContextFunc("pinMessage", c.tmplPinMessage(false))
 	c.addContextFunc("publishMessage", c.tmplPublishMessage)
 	c.addContextFunc("publishResponse", c.tmplPublishResponse)
-	c.addContextFunc("unpinMessage", c.tmplPinMessage(true))
 	c.addContextFunc("suppressEmbeds", c.tmplSuppressEmbeds(true))
+	c.addContextFunc("unpinMessage", c.tmplPinMessage(true))
 	c.addContextFunc("unsuppressEmbeds", c.tmplSuppressEmbeds(false))
 
 	// Message send functions
