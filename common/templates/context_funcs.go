db78d54fe080d68ab86430df5abc01a6f583cc8c
Henri <104494267+H1nr1@users.noreply.github.com>
2025-01-12 02:57:47 -0500

    Custom Commands: update ErrRegexCacheLimit

--- common/templates/context_funcs.go
+++ common/templates/context_funcs.go
@@ -24,7 +24,7 @@ import (
 var (
 	ErrTooManyCalls    = errors.New("too many calls to this function")
 	ErrTooManyAPICalls = errors.New("too many potential Discord API calls")
-	ErrRegexCacheLimit = errors.New("too many unique RegExes")
+	ErrRegexCacheLimit = errors.New("too many unique regular expressions (regex)")
 )
 
 func (c *Context) tmplSendDM(s ...interface{}) string {
