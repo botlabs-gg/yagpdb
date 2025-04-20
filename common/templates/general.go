d3ae4b9b562d16e07aed1a098b617d41bfb06631
Veda Maharaj <110698921+SoggySaussages@users.noreply.github.com>
2025-04-19 21:29:45 -0700

    templates: add suppress embeds to complexMessage(Edit) (#1873)
    Signed-off-by: SoggySaussages <vmdmaharaj@gmail.com>

--- common/templates/general.go
+++ common/templates/general.go
@@ -475,6 +475,11 @@ func CreateMessageSend(values ...interface{}) (*discordgo.MessageSend, error) {
 			} else {
 				msg.StickerIDs = append(msg.StickerIDs, ToInt64(val))
 			}
+		case "suppress_embeds":
+			if val == nil || val == false {
+				continue
+			}
+			msg.Flags |= discordgo.MessageFlagsSuppressEmbeds
 		default:
 			return nil, errors.New(`invalid key "` + key + `" passed to send message builder.`)
 		}
@@ -631,6 +636,11 @@ func CreateMessageEdit(values ...interface{}) (*discordgo.MessageEdit, error) {
 				}
 				msg.Components = append(msg.Components, discordgo.ActionsRow{[]discordgo.MessageComponent{menu}})
 			}
+		case "suppress_embeds":
+			if val == nil || val == false {
+				continue
+			}
+			msg.Flags |= discordgo.MessageFlagsSuppressEmbeds
 		default:
 			return nil, errors.New(`invalid key "` + key + `" passed to message edit builder`)
 		}
