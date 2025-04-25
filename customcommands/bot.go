4aadc2acf32158e34f2784c99b4cec664e7f7a52
SoggySaussages <vmdmaharaj@gmail.com>
2025-04-24 20:34:22 -0700

    Revert "cc/cc cmd: use components v2"
    This reverts commit 49ea3a7b8640fef8691e1c2fc191ff5debe4ccb9.

--- customcommands/bot.go
+++ customcommands/bot.go
@@ -403,9 +403,6 @@ var cmdListCommands = &commands.YAGCommand{
 			ccIDMaybeWithLink = fmt.Sprintf("[%d](%s)", cc.LocalID, cmdControlPanelLink(cc))
 		}
 
-		msgContent := discordgo.TextDisplay{}
-		msgFile := discordgo.ComponentFile{}
-
 		// Every message content-based custom command trigger has a numerical value less than 5
 		if cc.TriggerType < 5 || cc.TriggerType == int(CommandTriggerComponent) || cc.TriggerType == int(CommandTriggerModal) {
 			var header string
@@ -421,16 +418,8 @@ var cmdListCommands = &commands.YAGCommand{
 			}
 
 			if ccFile != nil {
-				msgContent.Content = header
+				msg.Content = header
 				msg.Files = []*discordgo.File{ccFile}
-				msgFile.File.URL = "attachment://" + ccFile.Name
-				msg.Flags |= discordgo.MessageFlagsIsComponentsV2
-
-				msg.Components = []discordgo.TopLevelComponent{
-					msgContent,
-					discordgo.Separator{},
-					msgFile,
-				}
 				return msg, nil
 			}
 
@@ -446,16 +435,9 @@ var cmdListCommands = &commands.YAGCommand{
 				header = fmt.Sprintf("#%s - Type: `%s` - Group: `%s` - Disabled: `%t` Public: `%t`", ccIDMaybeWithLink, CommandTriggerType(cc.TriggerType), groupMap[cc.GroupID.Int64], cc.Disabled, cc.Public)
 			}
 
-			msgContent.Content = header
+			msg.Content = header
 			msg.Files = []*discordgo.File{ccFile}
-			msgFile.File.URL = "attachment://" + ccFile.Name
-			msg.Flags |= discordgo.MessageFlagsIsComponentsV2
 
-			msg.Components = []discordgo.TopLevelComponent{
-				msgContent,
-				discordgo.Separator{},
-				msgFile,
-			}
 			return msg, nil
 
 		}
@@ -469,7 +451,6 @@ var cmdListCommands = &commands.YAGCommand{
 				ccIDMaybeWithLink, CommandTriggerType(cc.TriggerType), groupMap[cc.GroupID.Int64], cc.Disabled, cc.Public,
 				highlight, strings.Join(cc.Responses, "```\n```"))
 		}
-
 		return msg, nil
 	},
 }
