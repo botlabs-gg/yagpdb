960e7ce97e9ccf308462b68744e48feb8dc1e1d7
Rhyker Wells <a.rhykerw@gmail.com>
2025-07-07 11:40:21 +0000

    feat(templates): add getMember to functions

--- common/templates/context.go
+++ common/templates/context.go
@@ -760,6 +760,7 @@ func baseContextFuncs(c *Context) {
 
 	// Member functions
 	c.addContextFunc("getMember", c.tmplGetMember)
+	c.addContextFunc("getMembers", c.tmplGetMembers)
 	c.addContextFunc("getMemberVoiceState", c.tmplGetMemberVoiceState)
 	c.addContextFunc("editNickname", c.tmplEditNickname)
 
