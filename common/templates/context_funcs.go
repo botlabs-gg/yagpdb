a898f603b93685e6085e6ea88ef1573d720b631d
Ashish <51633862+ashishjh-bst@users.noreply.github.com>
2025-07-31 12:16:24 +0530

    Added a fallback to get thread info from discord (#1921)
    * Added a fallback to get thread info from discord in case thread is not present in state, this is plausible when threads are archived,  archieved threads are not kept in state
    
    * if thread id is in int, bypass converting it to args
    
    ---------
    
    Co-authored-by: Ashish <ashish@Ashishs-MacBook-Pro.local>

--- common/templates/context_funcs.go
+++ common/templates/context_funcs.go
@@ -1178,18 +1178,36 @@ func (c *Context) tmplGetThread(channel interface{}) (*CtxChannel, error) {
 		return nil, ErrTooManyAPICalls
 	}
 
-	cID := c.ChannelArg(channel)
+	var cID int64
+	if _, ok := channel.(int64); ok {
+		cID = channel.(int64)
+	} else {
+		cID = c.ChannelArg(channel)
+	}
+
 	if cID == 0 {
-		return nil, nil // dont send an error , a nil output would indicate invalid/unknown channel
+		return nil, fmt.Errorf("unknown or invalid thread")
 	}
 
 	cstate := c.GS.GetThread(cID)
-
-	if cstate == nil {
-		return nil, errors.New("thread not in state")
+	if cstate != nil {
+		return CtxChannelFromCS(cstate), nil
 	}
 
-	return CtxChannelFromCS(cstate), nil
+	logger.Warnf("thread %d for guild %d not in state, fetching form ", cID, c.GS.ID)
+	thread, err := common.BotSession.Channel(cID)
+	if err != nil {
+		logger.Errorf("failed to get thread %d for guild %d from discord", cID, c.GS.ID)
+		return nil, fmt.Errorf("failed to get thread %d", cID)
+	}
+
+	if thread == nil || thread.GuildID != c.GS.ID || !thread.Type.IsThread() {
+		logger.Errorf("Invalid thread %d for guild %d", cID, c.GS.ID)
+		return nil, fmt.Errorf("invalid thread id %d", cID)
+	}
+
+	tstate := dstate.ChannelStateFromDgo(thread)
+	return CtxChannelFromCS(&tstate), nil
 }
 
 func (c *Context) tmplThreadMemberAdd(threadID, memberID interface{}) string {
