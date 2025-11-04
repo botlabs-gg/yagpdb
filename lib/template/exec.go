5249a386af5f107f9e1ae9ba506ed69f0870e80d
Luca Zeuch <l-zeuch@email.de>
2025-11-04 17:56:41 +0100

    lib/template: don't evaluate '.' as float64 (#1971)
    This is taken directly from upstream at https://github.com/golang/go/commit/0f7b4e72a054f974489d8342cdae5a6a7ba7a31b
    with minor testing on my end via `evalcc`
    
    Unfortunately the patch does not apply cleanly to our codebase, so I had
    to resort to copy-and-paste, thereby obscuring authorship.
    
    Signed-off-by: Luca Zeuch <l-zeuch@email.de>

--- lib/template/exec.go
+++ lib/template/exec.go
@@ -712,7 +712,9 @@ func (s *state) idealConstant(constant *parse.NumberNode) reflect.Value {
 	switch {
 	case constant.IsComplex:
 		return reflect.ValueOf(constant.Complex128) // incontrovertible.
-	case constant.IsFloat && !isHexInt(constant.Text) && strings.ContainsAny(constant.Text, ".eEpP"):
+	case constant.IsFloat &&
+		!isHexInt(constant.Text) && !isRuneInt(constant.Text) &&
+		strings.ContainsAny(constant.Text, ".eEpP"):
 		return reflect.ValueOf(constant.Float64)
 	case constant.IsInt:
 		n := int(constant.Int64)
@@ -724,6 +726,10 @@ func (s *state) idealConstant(constant *parse.NumberNode) reflect.Value {
 		s.errorf("%s overflows int", constant.Text)
 	}
 	return zero
+}
+
+func isRuneInt(s string) bool {
+	return len(s) > 0 && s[0] == '\''
 }
 
 func isHexInt(s string) bool {
