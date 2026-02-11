c2461c2174b61044526f5a5f80466952af5ca99d
Ashish <51633862+ashishjh-bst@users.noreply.github.com>
2026-02-11 18:53:47 +0530

    return error when thread creation fails (#2026)

--- common/templates/context_funcs.go
+++ common/templates/context_funcs.go
@@ -1293,7 +1293,7 @@ func (c *Context) tmplCreateThread(channel, msgID, name interface{}, optionals .
 	}
 
 	if err != nil {
-		return nil, nil // dont send an error, a nil output would indicate invalid/unknown channel
+		return nil, err
 	}
 
 	tstate := dstate.ChannelStateFromDgo(ctxThread)
@@ -1520,7 +1520,7 @@ func (c *Context) tmplCreateForumPost(channel, name, content interface{}, option
 
 	thread, err := common.BotSession.ForumThreadStartComplex(cID, start, msgData)
 	if err != nil {
-		return nil, errors.New("unable to create forum post")
+		return nil, err
 	}
 
 	tstate := dstate.ChannelStateFromDgo(thread)
